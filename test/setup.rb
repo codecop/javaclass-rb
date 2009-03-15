3e3324fe1ddb7c3da5e5a68998fbbc6758197ea2
Peter Kofler <peter.kofler@code-cop.org>
2009-03-15 12:23:20 +0000

    more tests for class version and constant pool

--- test/setup.rb
+++ test/setup.rb
@@ -5,5 +5,10 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
 TEST_DATA_PATH = File.join(File.dirname(__FILE__), 'data')
 
+# Load the binary class data of given _name_ from the test path.
+def load_class(name)
+  File.open("#{TEST_DATA_PATH}/#{name}.class", 'rb') {|io| io.read } 
+end
+
 # require 'javaclass'
 # require File.join(File.dirname(__FILE__), %w[.. lib javaclass])
