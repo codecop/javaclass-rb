9f93582d3351e4a4bc1b27780e4e81dbcb255e63
Peter Kofler <peter.kofler@code-cop.org>
2009-03-15 00:29:22 +0000

    add global entry method

--- lib/javaclass.rb
+++ lib/javaclass.rb
@@ -1,1 +1,11 @@
 require 'javaclass/java_class_header'
+
+# Author::          Peter Kofler
+module JavaClass # :nodoc:
+  
+  # Convinience method to read and parse a class named _name_
+  def self.parse(name)
+    JavaClassHeader.new(File.open(name, 'rb') {|io| io.read } )
+  end
+  
+end
