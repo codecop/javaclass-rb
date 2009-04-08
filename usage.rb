4b9c60c882727f5baf2ddf12f90881f26ab07797
Peter Kofler <peter.kofler@code-cop.org>
2009-04-08 21:14:54 +0000

    new usage

--- usage.rb
+++ usage.rb
@@ -5,10 +5,13 @@ if __FILE__ == $0
   
   require 'javaclass'
   
-  clazz = JavaClass.parse('test/data/PublicClass.class')
-  puts clazz.version                        # => 50.0
-  puts clazz.constant_pool.items[1]         # => packagename/PublicClass
-  puts clazz.accessible?                    # => true 
+  clazz = JavaClass.parse('test/data/AccessFlagsTestPublic.class')
+  puts clazz.version                          # => 50.0
+  puts clazz.constant_pool.items[1]           # => packagename/AccessFlagsTestPublic
+  puts clazz.access_flags.public?             # => true
+  puts clazz.this_class                       # => packagename/AccessFlagsTestPublic
+  puts clazz.super_class                      # => java/lang/Object
+  puts clazz.references.referenced_methods[0] # => java/lang/Object.<init>:()V
   
 end
 
