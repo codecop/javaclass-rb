8aa33e5dff4c3d9cc083509c45ce95df77f44b94
Peter Kofler <peter.kofler@code-cop.org>
2009-10-07 22:38:43 +0000

    add java home classpath (only test)

--- test/test_java_home_classpath.rb
+++ test/test_java_home_classpath.rb
@@ -1,12 +1,49 @@
 require File.dirname(__FILE__) + '/setup'
-# require 'javaclass/classpath/parser'
+require 'javaclass/classpath/java_home_classpath'
 
-class TestJavaHomeClasspath < Test::Unit::TestCase
+module TestJavaClass
+  module TestClasspath
+    
+    class TestJavaHomeClasspath < Test::Unit::TestCase
+      
+      def setup
+        @cpe = JavaClass::Classpath::JavaHomeClasspath.new("#{TEST_DATA_PATH}/JavaHomeClasspathTest/jre")
+      end
+      
+      def test_count
+        assert_equal(1, @cpe.count)
 
-  def test_some
-#    assert_equal([], "./abc;c:\\temp;", )
-#    assert_equal(49, "212".u1(1))
-#    assert_equal(50, "212".u1(2))
+        @cpe = JavaClass::Classpath::JavaHomeClasspath.new("#{TEST_DATA_PATH}/JavaHomeClasspathTest/jdk")
+        assert_equal(1, @cpe.count)
+      end
+      
+      def test_additional_classpath
+        assert_equal([], @cpe.additional_classpath)
+        
+        @cpe = JavaClass::Classpath::JavaHomeClasspath.new("#{TEST_DATA_PATH}/JavaHomeClasspathTest/jre-ext")
+        assert_equal(["#{TEST_DATA_PATH}/JavaHomeClasspathTest/jre-ext/lib/ext/ext.jar"], @cpe.additional_classpath)
+      end
+      
+      def test_jar_eh
+        assert(@cpe.jar?)
+      end
+      
+      def test_includes_eh
+        assert(@cpe.includes?('ClassVersionTest10.class'))
+        assert(@cpe.includes?('ClassVersionTest10'))
+        assert(!@cpe.includes?('ClassVersionTest11.class'))
+        assert(!@cpe.includes?('ClassVersionTest11'))
+      end
+      
+      def test_load_binary
+        assert_equal(load_class('ClassVersionTest10'), @cpe.load_binary('ClassVersionTest10'))
+      end
+      
+      def test_names
+        assert_equal(['ClassVersionTest10.class'], @cpe.names)
+      end
+      
+    end
+    
   end
-
 end
