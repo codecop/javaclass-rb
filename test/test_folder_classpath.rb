5055f29453ec0e14b32efcdc10716e6b4cca9dee
Peter Kofler <peter.kofler@code-cop.org>
2009-10-07 22:38:18 +0000

    add folder classpath

---
+++ test/test_folder_classpath.rb
@@ -0,0 +1,47 @@
+require File.dirname(__FILE__) + '/setup'
+require 'javaclass/classpath/folder_classpath'
+
+module TestJavaClass
+  module TestClasspath
+    
+    class TestFolderClasspath < Test::Unit::TestCase
+      
+      def setup
+        @cpe = JavaClass::Classpath::FolderClasspath.new("#{TEST_DATA_PATH}/JarClasspathTestFolder")
+      end
+      
+      def test_count
+        assert_equal(2, @cpe.count)
+      end
+      
+      def test_additional_classpath
+        # dummy
+        assert_equal([], @cpe.additional_classpath)
+      end
+      
+      def test_jar_eh
+        # dummy
+        assert(!@cpe.jar?)
+      end
+      
+      def test_includes_eh
+        assert(@cpe.includes?('ClassVersionTest10.class'))
+        assert(@cpe.includes?('ClassVersionTest10'))
+        assert(!@cpe.includes?('ClassVersionTest11.class'))
+        assert(!@cpe.includes?('ClassVersionTest11'))
+        assert(@cpe.includes?('package/ClassVersionTest11.class'))
+        assert(@cpe.includes?('package/ClassVersionTest11'))
+      end
+      
+      def test_load_binary
+        assert_equal(load_class('ClassVersionTest10'), @cpe.load_binary('ClassVersionTest10'))
+      end
+      
+      def test_names
+        assert_equal(['ClassVersionTest10.class', 'package/ClassVersionTest11.class'], @cpe.names)
+      end
+      
+    end
+    
+  end
+end
