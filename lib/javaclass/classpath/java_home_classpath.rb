e36c03f1be3fa2b66ee95bfe2a1d05472184a320
Peter Kofler <peter.kofler@code-cop.org>
2009-10-09 22:53:44 +0000

    implement

--- lib/javaclass/classpath/java_home_classpath.rb
+++ lib/javaclass/classpath/java_home_classpath.rb
@@ -1,23 +1,37 @@
-require File.expand_path(File.dirname(__FILE__) + '/jar_classpath')
+require 'javaclass/classpath/jar_classpath'
 
 module JavaClass
   module Classpath # :nodoc:
     
-    # Abstraction of the Java boot CLASSPATH. 
+    # Abstraction of the Java boot CLASSPATH. May return additional classpath elements for endorsed libs. 
     # Author::   Peter Kofler
     class JavaHomeClasspath < JarClasspath
       
       # Return the list of classnames found in this _javahome_ . 
       def initialize(javahome)
-        @javahome = javahome
-        # TODO seatch for rt.jar, invoke super
+        if FileTest.exist? rtjar=File.join(javahome, 'lib', 'rt.jar')
+          super(rtjar)
+        elsif FileTest.exist? rtjar=File.join(javahome, 'jre', 'lib', 'rt.jar')
+          super(rtjar)
+        else
+          raise IOError, "rt.jar not found in java home #{javahome}"
+        end
+        @lib = File.dirname(rtjar)
       end
       
-      # Return list of additional classpath elements relative to this javahome.
+      # Return list of additional classpath elements, e.g. endorsed libs found in this Java Home.
       def additional_classpath
-        found = super
-        # TODO search ext folder for endorsed jars and return full names
-        found
+        list = super
+        
+        if FileTest.exist? ext=File.join(@lib, 'ext') 
+          current = Dir.getwd
+          Dir.chdir ext
+          
+          list += Dir['*.jar'].collect { |jar| File.join(ext, jar) }
+          
+          Dir.chdir current
+        end
+        list
       end
       
     end
