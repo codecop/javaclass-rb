c1dfbb61de0a81f9f152ecc4e6fead2e9a32d677
Peter Kofler <peter.kofler@code-cop.org>
2010-09-23 20:39:47 +0200

    add planned ports, fragments and todos

---
+++ lib/javaclass/metric/class_usage.rb
@@ -0,0 +1,39 @@
+# add the lib to the load path
+$:.unshift File.join(File.dirname(__FILE__), 'lib')
+
+require 'javaclass'
+include JavaClass
+
+JDK_PACKAGES = %w| java javax.accessibility javax.activity javax.crypto javax.imageio javax.jnlp javax.management 
+                   javax.naming javax.net javax.print javax.rmi javax.script javax.security.auth javax.security.cert
+                   javax.security.sasl javax.sound.midi javax.sound.sampled javax.sql javax.swing javax.transaction
+                   javax.xml org.ietf.jgss org.w3c.dom org.xml.sax|.collect { |pkg| /^#{pkg}\./ }
+                     
+def in_jdk?(name)
+  JDK_PACKAGES.find { |regex| name =~ regex } != nil
+end                     
+
+def imported_types(classes)
+  own_classes = classes.names.collect { |name| name.gsub('/', '.').sub(/\.class$/, '')}.sort 
+  
+  imported = classes.names.collect do |name|
+    clazz = JavaClassHeader.new(classes.load_binary(name))
+    clazz.references.used_classes.collect { |c| c.to_s.gsub('/', '.') }  
+  end.flatten.uniq.sort
+  
+  imported.reject { |name| in_jdk?(name) } - own_classes
+end
+
+# TODO XML Spring auslesen und classes hinzufügen
+
+classes = JavaClass::Classpath::FolderClasspath.new("./target/classes")
+own = classes.names.collect { |name| name.gsub('/', '.').sub(/\.class$/, '')}.sort
+used = imported_types(classes)
+puts "---------- used types" 
+puts used
+
+test_classes = JavaClass::Classpath::FolderClasspath.new("./target/test-classes")
+test = imported_types(test_classes) - own - used 
+puts "---------- for tests" 
+puts test
+
---
+++ test/test_jar_class_list.rb
@@ -0,0 +1,89 @@
+#! ruby
+require 'test/unit'
+require File.expand_path(File.dirname(__FILE__) + '/jar_class_list')
+
+class TestJarClassList < Test::Unit::TestCase
+  
+  JAR = File.expand_path("#{File.dirname(__FILE__)}/../../test/demo.jar")
+  
+  PACKAGE_CLASS = "packagename/PackageClass.class"
+  INNER_CLASS = "packagename/PublicClass$InnerClass.class"
+  PUBLIC_CLASS = "packagename/PublicClass.class"
+  PUBLIC_INTERFACE = "packagename/PublicInterface.class"
+  
+  def setup
+    @cs = JarClassSearcher.new
+  end
+  
+  def test_find_jars
+    jars = @cs.find_jars('test')
+    assert_equal(1, jars.size)
+    assert_equal(JAR, jars[0])
+  end
+  
+  def test_list_classes
+    @cs.skip_inner_classes = false
+    
+    classes = @cs.list_classes(JAR)
+    assert_equal(4, classes.size)
+    assert_equal(INNER_CLASS, classes[0])
+    assert_equal(PUBLIC_CLASS, classes[1])
+    assert_equal(PUBLIC_INTERFACE, classes[2])
+    assert_equal(PACKAGE_CLASS, classes[3])
+    
+    @cs.skip_inner_classes = true
+    classes = @cs.list_classes(JAR)
+    assert_equal(3, classes.size)
+    assert_equal(PUBLIC_CLASS, classes[0])
+    assert_equal(PUBLIC_INTERFACE, classes[1])
+    assert_equal(PACKAGE_CLASS, classes[2])
+  end
+  
+  def test_filter
+    @cs.filters = ['packagename/']
+    
+    classes = @cs.list_classes(JAR)
+    assert_equal(0, classes.size)
+  end
+  
+  def test_if_public
+    assert(!@cs.public?(JAR, PACKAGE_CLASS))
+    assert(!@cs.public?(JAR, INNER_CLASS))
+    assert(@cs.public?(JAR, PUBLIC_CLASS))
+  end
+  
+  class MockList 
+    attr_reader :versions
+    attr_reader :modifiers
+    attr_reader :entries
+    
+    def initialize
+      @versions = []
+      @modifiers = []
+      @size = 0
+      @entries = []
+    end
+    
+    def add_class(entry, is_public, version)
+      @entries << entry
+      @modifiers << is_public
+      @versions << version
+    end
+    
+    def size
+      @entries.size
+    end
+  end
+  
+  def test_overall
+    @cs.skip_package_classes = true
+    
+    list = @cs.compile_list(2, 'test', MockList.new )
+    
+    assert_equal(2, list.size)
+    assert_equal([PUBLIC_CLASS, PUBLIC_INTERFACE], list.entries)
+    assert_equal([true, true], list.modifiers)
+    assert_equal([2, 2], list.versions)
+  end
+  
+end
