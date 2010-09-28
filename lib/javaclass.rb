063df511fb9b44fdea656d40ff6dea16fcaeab13
Peter Kofler <peter.kofler@code-cop.org>
2010-09-28 21:19:22 +0200

    update facade methods and usage doc

--- lib/javaclass.rb
+++ lib/javaclass.rb
@@ -6,18 +6,38 @@ require 'javaclass/classpath/composite_classpath'
 # Author::          Peter Kofler
 module JavaClass
 
-    # Read and disassemble the given class called _name_ (full file name).
-    def self.parse(name)
-      ClassFile::JavaClassHeader.new(File.open(name, 'rb') {|io| io.read } )
+    # Read and disassemble the given class from _filename_ (full file name).
+    def self.load_fs(filename)
+      disassemble(File.open(filename, 'rb') { |io| io.read } )
+    end
+    
+    def self.parse(filename)
+      warn 'Deprecated method JavaClass::parse will be removed in next release. Use method load_fs instead.'
+      load_fs(filename)
+    end
+
+    # Read and disassemble the given class _classname_ from _classpath_ .
+    def self.load_cp(classname, classpath)
+      disassemble(classpath.load_binary(classname))
+    end
+
+    # Read and disassemble the given class inside _data_ (byte data).
+    def self.disassemble(data)
+      ClassFile::JavaClassHeader.new(data)
+    end
+
+    # Parse the given path variable _path_ and return a chain of class path elements.
+    def self.classpath(path)
+      full_classpath(nil, path)
     end
     
     # Parse and scan the system classpath. Needs +JAVA_HOME+ set. Uses environment +CLASSPATH+ if set.
-    def self.system_classpath
-      classpath(ENV['JAVA_HOME'], ENV['CLASSPATH'])
+    def self.environment_classpath
+      full_classpath(ENV['JAVA_HOME'], ENV['CLASSPATH'])
     end
-    
-    # Parse the given path _path_ and return a chain of class path elements.
-    def self.classpath(javahome, path='')
+
+    # Parse the given path variable _path_ and return a chain of class path elements together with _javahome_ if any.
+    def self.full_classpath(javahome, path='')
       cp = Classpath::CompositeClasspath.new
       cp.add_element(Classpath::JavaHomeClasspath.new(javahome)) if javahome
       path.split(File::PATH_SEPARATOR).each { |cpe| cp.add_file_name(cpe) } if path
--- usage.rb
+++ usage.rb
@@ -4,13 +4,21 @@ $:.unshift File.join(File.dirname(__FILE__), 'lib')
 if __FILE__ == $0
   
   require 'javaclass'
+
+  # load the class directly from the file system
+  clazz = JavaClass.load_fs('test/data/access_flags/AccessFlagsTestPublic.class')
   
-  clazz = JavaClass.parse('test/data/access_flags/AccessFlagsTestPublic.class')
+  # better lookup the class from some classpath
+  classpath = JavaClass.classpath('test/data/access_flags')
+  clazz = JavaClass.load_cp('AccessFlagsTestPublic', classpath)
+  
   puts clazz.version                          # => 50.0
   puts clazz.constant_pool.items[1]           # => packagename/AccessFlagsTestPublic
   puts clazz.access_flags.public?             # => true
   puts clazz.this_class                       # => packagename/AccessFlagsTestPublic
+  puts clazz.this_class.to_java_file          # => packagename/AccessFlagsTestPublic.java
   puts clazz.super_class                      # => java/lang/Object
+  puts clazz.super_class.to_classname         # => java.lang.Object
   puts clazz.references.referenced_methods[0] # => java/lang/Object.<init>:()V
   
 end
