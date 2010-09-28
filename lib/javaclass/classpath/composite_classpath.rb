19a371ad72afb0664d3252edbab7ce1ec658f7b6
Peter Kofler <peter.kofler@code-cop.org>
2010-09-28 14:36:59 +0200

    simplify class_list, some tests still broken

--- lib/javaclass/classpath/composite_classpath.rb
+++ lib/javaclass/classpath/composite_classpath.rb
@@ -3,23 +3,40 @@ require 'javaclass/classpath/jar_classpath'
 require 'javaclass/classpath/folder_classpath'
 
 module JavaClass
-  module Classpath 
-    
-    # List of class path elements. 
+  module Classpath
+    # List of class path elements.
     # Author::   Peter Kofler
     class CompositeClasspath
-      
-      # Return the list of classnames found in this path. 
+      # Return the list of classnames found in this path.
       def initialize
         @elements = []
       end
-      
+
       # Return the classpath elements of this path.
       def elements
         @elements.dup
       end
-      
-      # Add the _name_ class path file name to the list.
+
+      # Search the given _path_ recursievely for zips or jars. Add all found jars to this classpath.
+      def find_jars(path)
+        current = Dir.getwd
+        begin
+          Dir.chdir File.expand_path(path)
+
+          Dir['*'].collect do |name|
+            if FileTest.directory?(name)
+              find_jars(name)
+            elsif name =~ /\.jar$|\.zip$/
+              add_file_name File.expand_path(name)
+            end
+          end
+
+        ensure
+          Dir.chdir current
+        end
+      end
+
+      # Add the _name_ class path file/folder name to the list.
       def add_file_name(name)
         if FileTest.directory? name
           add_element(FolderClasspath.new(name))
@@ -27,52 +44,57 @@ module JavaClass
           add_element(JarClasspath.new(name))
         end
       end
-      
-      # Add the _elem_ classpath element to the list. 
+
+      # Add the _elem_ classpath element to the list.
       def add_element(elem)
-        @elements << elem
-        elem.additional_classpath.each {|acpe| add_element(JarClasspath.new(acpe)) }
+        @elements << elem unless @elements.find { |cpe| cpe .to_s == elem.to_s }
+        elem.additional_classpath.each do |acpe|
+          # referred classpath elements may be missing
+          if FileTest.exist? acpe
+            add_element(JarClasspath.new(acpe)) 
+          end 
+        end
       end
-      
+
       # Return false.
       def jar?
         false
       end
-      
+
       # Return an empty array.
       def additional_classpath
         []
       end
-      
+
       # Return the list of class names found in this classpath.
       def names
         @elements.collect { |e| e.names }.flatten.uniq
       end
-      
+
       # Return if _classname_ is included in this classpath.
       def includes?(classname)
         @elements.find { |e| e.includes?(classname) } != nil
       end
-      
+
       # Load the binary data of the file name or class name _classname_ from this classpath.
       def load_binary(classname)
         found = @elements.find { |e| e.includes?(classname) }
         if (found)
           return found.load_binary(classname)
         end
-        raise "class #{classname} not found in classpath #{to_s}" 
+        raise "class #{classname} not found in classpath #{to_s}"
       end
-      
+
       # Return the number of classes in this jar.
       def count
         @elements.inject(0) { |s,e| s + e.count }
       end
-      
+
       def to_s
-        @elements.collect { |e| e.to_s }
+        @elements.collect { |e| e.to_s }.join(',')
       end
-      
+
     end
-    
+
   end
 end
--- lib/javaclass/classpath/folder_classpath.rb
+++ lib/javaclass/classpath/folder_classpath.rb
@@ -2,67 +2,68 @@ require 'javaclass/java_name'
 
 module JavaClass
   module Classpath # :nodoc:
-    
-    # Abstraction of a folder on the CLASSPATH. 
+    # Abstraction of a folder on the CLASSPATH.
     # Author::   Peter Kofler
     class FolderClasspath
-      
-      # Return the list of classnames found in this _folder_ . 
+      # Return the list of classnames found in this _folder_ .
       def initialize(folder)
         @folder = folder
         raise IOError, "folder #{@folder} not found" if !FileTest.exist? @folder
         raise "#{@folder} is no folder" if !FileTest.directory? @folder
         @classes = list_classes.collect { |cl| cl.to_javaname }
       end
-      
+
       # Return false.
       def jar?
         false
       end
-      
+
       # Return an empty array.
       def additional_classpath
         []
       end
-      
+
       # Return the list of class names found in this folder.
       def names
         @classes.dup
       end
-      
+
       # Return if _classname_ is included in this folder.
       def includes?(classname)
         @classes.include?(normalize(classname))
       end
-      
+
       # Load the binary data of the file name or class name _classname_ from this jar.
       def load_binary(classname)
         raise "class #{classname} not found in #{@folder}" unless includes?(classname)
         File.open(File.join(@folder, normalize(classname)), 'rb') {|io| io.read }
       end
-      
+
       # Return the number of classes in this folder.
       def count
         @classes.size
       end
-      
+
       def to_s
         @folder
       end
-      
-      private 
-      
-      # Return the list of classnames (in fact file names) found in this folder. 
+
+      private
+
+      # Return the list of classnames (in fact file names) found in this folder.
       def list_classes
         current = Dir.getwd
-        Dir.chdir @folder
-        
-        list = Dir['**/*.class']        
-        
-        Dir.chdir current
+        begin
+          Dir.chdir @folder
+
+          list = Dir['**/*.class']
+
+        ensure
+          Dir.chdir current
+        end
         list.sort
       end
-      
+
       # Normalize the file name or class name _classname_ to be a file name in the jar.
       def normalize(classname)
         if classname !~ /\.class$/
@@ -71,8 +72,8 @@ module JavaClass
           classname
         end
       end
-      
+
     end
-    
+
   end
 end
