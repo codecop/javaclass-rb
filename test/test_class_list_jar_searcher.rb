f4678c27ad0c1aff91f5e924d6c59139073ff1e1
Peter Kofler <peter.kofler@code-cop.org>
2010-09-28 18:17:25 +0200

    rename class list searcher

---
+++ test/test_class_list_jar_searcher.rb
@@ -0,0 +1,93 @@
+require File.dirname(__FILE__) + '/setup'
+require 'javaclass/analyse/class_list_jar_searcher'
+require 'javaclass/classpath/jar_classpath'
+
+module TestJavaClass
+  module TestAnalyse
+    class TestClassListJarSearcher < Test::Unit::TestCase
+      
+      PACKAGE_CLASS = "packagename/PackageClass.class"
+      INNER_CLASS = "packagename/PublicClass$InnerClass.class"
+      PUBLIC_CLASS = "packagename/PublicClass.class"
+      PUBLIC_INTERFACE = "packagename/PublicInterface.class"
+      CLASSES = [INNER_CLASS, PUBLIC_CLASS, PUBLIC_INTERFACE, PACKAGE_CLASS]
+      
+      def setup
+        @cs = JavaClass::Analyse::ClassListJarSearcher.new
+      end
+      
+      def test_filter_classes
+        @cs.skip_inner_classes = false
+        classes = @cs.filter_classes(CLASSES)
+        assert_equal(4, classes.size)
+        assert_equal(INNER_CLASS, classes[0])
+        assert_equal(PUBLIC_CLASS, classes[1])
+        assert_equal(PUBLIC_INTERFACE, classes[2])
+        assert_equal(PACKAGE_CLASS, classes[3])
+      end
+      
+      def test_skip_inner_classes_equals
+        # @cs.skip_inner_classes = true # default
+        classes = @cs.filter_classes(CLASSES)
+        assert_equal(3, classes.size)
+        assert_equal(PUBLIC_CLASS, classes[0])
+        assert_equal(PUBLIC_INTERFACE, classes[1])
+        assert_equal(PACKAGE_CLASS, classes[2])
+      end
+      
+      def test_filters_equals
+        @cs.filters = ['packagename/']
+        classes = @cs.filter_classes(CLASSES)
+        assert_equal(0, classes.size)
+      end
+      
+      def test_public_eh
+        @cpe = JavaClass::Classpath::JarClasspath.new(File.expand_path("#{TEST_DATA_PATH}/jar_class_list/JarClassListTest.jar"))
+        assert(!@cs.public?(@cpe, PACKAGE_CLASS))
+        assert(!@cs.public?(@cpe, INNER_CLASS))
+        assert(@cs.public?(@cpe, PUBLIC_CLASS))
+      end
+      
+      class MockList # ZenTest SKIP
+        attr_reader :versions
+        attr_reader :modifiers
+        attr_reader :entries
+        def initialize
+          @versions = []
+          @modifiers = []
+          @size = 0
+          @entries = []
+        end
+        
+        def add_class(entry, is_public, version)
+          @entries << entry
+          @modifiers << is_public
+          @versions << version
+        end
+        
+        def size
+          @entries.size
+        end
+      end
+      
+      def test_compile_list
+        list = @cs.compile_list(2, "#{TEST_DATA_PATH}/jar_class_list", MockList.new )
+        
+        assert_equal(3, list.size)
+        assert_equal([PACKAGE_CLASS, PUBLIC_CLASS, PUBLIC_INTERFACE], list.entries)
+        assert_equal([false, true, true], list.modifiers)
+        assert_equal([2, 2, 2], list.versions)
+      end
+      
+      def test_skip_package_classes_equals
+        @cs.skip_package_classes = true
+        list = @cs.compile_list(3, "#{TEST_DATA_PATH}/jar_class_list", MockList.new )
+        
+        assert_equal(2, list.size)
+        assert_equal([PUBLIC_CLASS, PUBLIC_INTERFACE], list.entries)
+      end
+      
+    end
+    
+  end
+end
--- test/ts_all_tests.rb
+++ test/ts_all_tests.rb
@@ -3,7 +3,6 @@ require 'test/unit/testsuite'
 require 'test/unit' if $0 == __FILE__
 
 require File.dirname(__FILE__) + '/test_string_ux'
-
 # require File.dirname(__FILE__) + '/test_class_magic'
 require File.dirname(__FILE__) + '/test_class_version'
 require File.dirname(__FILE__) + '/test_base'
@@ -15,34 +14,38 @@ require File.dirname(__FILE__) + '/test_jar_classpath'
 require File.dirname(__FILE__) + '/test_folder_classpath'
 require File.dirname(__FILE__) + '/test_java_home_classpath'
 require File.dirname(__FILE__) + '/test_composite_classpath'
+require File.dirname(__FILE__) + '/test_class_list_jar_searcher'
 require File.dirname(__FILE__) + '/test_java_name.rb'
-require File.dirname(__FILE__) + '/test_javaclass.rb'
+require File.dirname(__FILE__) + '/test_facade.rb'
 
 class TestSuite_AllTests
   def self.suite
     suite = Test::Unit::TestSuite.new("Ruby Java Class")
-
-    # Java class parser
+    
     suite << TestString.suite
+    
+    # Java class parser
     suite << TestJavaClass::TestClassFile::TestClassVersion.suite
     suite << TestJavaClass::TestClassFile::TestConstants::TestBase.suite
     suite << TestJavaClass::TestClassFile::TestConstantPool.suite
     suite << TestJavaClass::TestClassFile::TestReferences.suite
     suite << TestJavaClass::TestClassFile::TestAccessFlags.suite
     suite << TestJavaClass::TestClassFile::TestJavaClassHeader.suite
-
+    
     # classpath parser
     suite << TestJavaClass::TestClasspath::TestJarClasspath.suite
     suite << TestJavaClass::TestClasspath::TestFolderClasspath.suite
     suite << TestJavaClass::TestClasspath::TestJavaHomeClasspath.suite
     suite << TestJavaClass::TestClasspath::TestCompositeClasspath.suite
-
+    
+    # analyse
+    suite << TestJavaClass::TestAnalyse::TestClassListJarSearcher.suite
+    
     # general
     suite << TestJavaClass::TestJavaName.suite
-    suite << TestJavaClass::TestSelf.suite
-
+    suite << TestJavaClass::TestFacade.suite
+    
     return suite
   end
-
+  
 end
-
