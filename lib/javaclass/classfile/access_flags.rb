42d00e8cc76de404191e1672ff88c048ac4a8ce8
Peter Kofler <peter.kofler@code-cop.org>
2010-09-28 22:18:51 +0200

    woraround for JDK 1.1

--- lib/javaclass/classfile/access_flags.rb
+++ lib/javaclass/classfile/access_flags.rb
@@ -19,7 +19,12 @@ module JavaClass
       def initialize(data, pos)
         @flags = data.u2(pos)
         raise "inconsistent flags #{@flags} (abstract and final)" if abstract? && final?
-        raise "inconsistent flags #{@flags} (interface not abstract or final)" if interface? && (!abstract? || final?)
+        if interface? && !abstract?
+          # JDK 1.0 and 1.1 do have non abstract interfaces, fix it
+          @flags = @flags | ACC_ABSTRACT
+        end
+        raise "inconsistent flags #{@flags} (interface not abstract)" if interface? && !abstract?
+        raise "inconsistent flags #{@flags} (interface is final)" if interface? && final?
         raise "inconsistent flags #{@flags} (other value #{@flags & ACC_OTHER})" if (@flags & ACC_OTHER) != 0
       end
       
