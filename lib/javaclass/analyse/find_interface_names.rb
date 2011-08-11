9c40c4416f73bd1dfaea464f18864a8a3854304a
Peter Kofler <peter.kofler@code-cop.org>
2011-08-11 20:25:33 +0200

    add proto tool interface names

---
+++ lib/javaclass/analyse/find_interface_names.rb
@@ -0,0 +1,18 @@
+# add the lib of this gem to the load path
+$:.unshift File.dirname(File.dirname(File.dirname(__FILE__)))
+
+require 'javaclass'
+include JavaClass
+
+if __FILE__ == $0
+  
+  if ARGV.empty?
+    puts "#{__FILE__} <base folder>"
+    puts "scan all Java cass files recursively and find interface names."
+    exit
+  end
+
+  cp = JavaClass.classpath(ARGV[0])
+  puts cp.names.collect { |name| JavaClass.load_cp(name, cp) }.find_all { |clazz| clazz.access_flags.interface? && clazz.access_flags.annotation? }
+
+end
