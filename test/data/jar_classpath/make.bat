31490836518a12cfff9faf5393d971ec1b8af74a
Peter Kofler <peter.kofler@code-cop.org>
2011-08-12 01:07:53 +0200

    refresh test data, there was crap inside

--- test/data/jar_classpath/make.bat
+++ test/data/jar_classpath/make.bat
@@ -3,12 +3,13 @@ if exist JarClasspathTest*.* del JarClasspathTest*.*
 md JarClasspathTestFolder\package
 copy ..\class_version\ClassVersionTest10.class JarClasspathTestFolder
 copy ..\class_version\ClassVersionTest11.class JarClasspathTestFolder\package
+copy ..\class_version\ClassVersionTest11.class .
 
 cd JarClasspathTestFolder
 jar -cf ..\JarClasspathTest.jar .
-cd ..
 
-call winzip -a JarClasspathTest.zip ClassVersionTest10.class
+call 7za a -bd -tzip ..\JarClasspathTest.zip ClassVersionTest10.class
+cd ..
 
 echo Class-Path: JarClasspathTest.jar > manifest.txt
 jar -cfm JarClasspathTestManifest.jar manifest.txt ClassVersionTest11.class
@@ -20,5 +21,8 @@ echo Class-Path: lib/httpunit-1.6.2.jar lib/nekohtml-0.9.1.jar             >> ma
 echo    lib/xercesImpl-2.5.jar lib/js-1.5R4.1.jar>> manifest.txt
 jar -cfm JarClasspathTestMultiManifest.jar manifest.txt ClassVersionTest11.class
 del manifest.txt
+
+rmdir /S /Q JarClasspathTestFolder
+del *.class
 
 pause
