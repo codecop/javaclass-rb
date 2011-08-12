e2e061ac1a21d35ab1b7e9bafcd04f68ed63b2dc
Peter Kofler <peter.kofler@code-cop.org>
2011-08-12 03:02:42 +0200

    cleanup small tool

--- lib/javaclass/metric/ccd.rb
+++ lib/javaclass/metric/ccd.rb
@@ -1,20 +1,20 @@
-# add the lib to the load path
-$:.unshift File.join(File.dirname(__FILE__), 'lib')
-
-# TODO cleanup this code fragment and make a proper class 
+# add the lib of this gem to the load path
+$:.unshift File.dirname(File.dirname(File.dirname(__FILE__)))
 
 require 'javaclass'
 include JavaClass
 
+# TODO cleanup this code fragment and make a proper class
+
 def process_class(name, already=[], intend=0)
-  file_name = 'C:\JavaDev\classes\\' + name.gsub(/\./,'\\') + '.class'
+  file_name = 'C:\JavaDev\classes\\' + name.dot_to_slash + CLASS
   return if !FileTest.exist? file_name
   # p file_name
   clazz = JavaClass.parse(file_name)
   # p clazz
   puts " "*intend + clazz.this_class
-  
-  imported = clazz.references.used_classes.collect { |c| c.to_s }.sort
+
+  imported = clazz.references.used_classes.collect { |c| c.full_name }.sort
   new_used = imported - already
   already << new_used
   already.flatten!
@@ -24,12 +24,12 @@ def process_class(name, already=[], intend=0)
 end
 
 def process_package(folder, already=[], intend=0)
-  file_name = 'C:\JavaDev\classes\\' + folder.gsub(/\./,'\\')
+  file_name = 'C:\JavaDev\classes\\' + folder.dot_to_slash
   cwd = Dir.getwd
   Dir.chdir file_name
   files = Dir['*']
   files.each do |c|
-    if c =~ /\.class$/
+    if c =~ CLASS_REGEX
       process_class(folder + '.' + c[/^[^.]+/], already, intend)
     elsif FileTest.directory? c
       puts " "*intend+ '-----'
@@ -39,30 +39,33 @@ def process_package(folder, already=[], intend=0)
   Dir.chdir cwd
 end
 
-# total = []
-# #process_class('at/spardat/krimiaps/service/client/service/impl/ClientSmeSearchServiceImpl', total)
-# #process_class('at/spardat/krimiaps/service/client/service/impl/ClientPrivateSearchServiceImpl', total)
-#
-# # --- common
-# #process_package('at.spardat.krimicee.common', total) #
-# #process_package('at.spardat.krimiaps.common', total) # -> 79 ok
-# #process_package('at.spardat.common', total) # 146, in sich, koennte nach common
-# #process_package('at.spardat.integrator', total) #
-# # DatabaseAwareComponent -> ganze DB
-# #process_package('at.spardat.krimicee.util', total)
-# # ItoTestApsSchema -> ganze DB
-# # process_package('at.spardat.krimicee.lib', total)
-# # KrimiLock -> ganze DB
-#
-# #process_package('at.spardat.krimicee.dom', total)
-# #process_package('at.spardat.krimicee.db', total)
-# # KrimiLoanService, PrintHelper ->
-# #process_package('at.spardat.krimicee.dao', total)
-# #process_package('at.spardat.krimicee.dao', total)
-# #process_package('at.spardat.krimicee.entity', total)
-# #process_package('at.spardat.krimicee.entity', total)
-# #process_package('at.spardat.krimicee.eo', total)
-# #process_package('at.spardat.krimicee.sql', total)
-# #process_package('at.spardat.krimicee.model', total) # 1647
-#
-# puts total.size
+if __FILE__ == $0
+
+  # total = []
+  #process_class('at/spardat/krimiaps/service/client/service/impl/ClientSmeSearchServiceImpl', total)
+  #process_class('at/spardat/krimiaps/service/client/service/impl/ClientPrivateSearchServiceImpl', total)
+
+  # --- common
+  #process_package('at.spardat.krimicee.common', total) #
+  #process_package('at.spardat.krimiaps.common', total) # -> 79 ok
+  #process_package('at.spardat.common', total) # 146, in sich, koennte nach common
+  #process_package('at.spardat.integrator', total) #
+  # DatabaseAwareComponent -> ganze DB
+  #process_package('at.spardat.krimicee.util', total)
+  # ItoTestApsSchema -> ganze DB
+  # process_package('at.spardat.krimicee.lib', total)
+  # KrimiLock -> ganze DB
+
+  #process_package('at.spardat.krimicee.dom', total)
+  #process_package('at.spardat.krimicee.db', total)
+  # KrimiLoanService, PrintHelper ->
+  #process_package('at.spardat.krimicee.dao', total)
+  #process_package('at.spardat.krimicee.dao', total)
+  #process_package('at.spardat.krimicee.entity', total)
+  #process_package('at.spardat.krimicee.entity', total)
+  #process_package('at.spardat.krimicee.eo', total)
+  #process_package('at.spardat.krimicee.sql', total)
+  #process_package('at.spardat.krimicee.model', total) # 1647
+
+  # puts total.size
+end
