214521bd331c8939bc6f5c595a8405bdefae008e
Peter Kofler <peter.kofler@code-cop.org>
2011-08-12 03:02:08 +0200

    remove redundant qualifiers

--- lib/javaclass/analyse/find_interface_names.rb
+++ lib/javaclass/analyse/find_interface_names.rb
@@ -5,7 +5,7 @@ require 'javaclass'
 include JavaClass
 
 if __FILE__ == $0
-  
+
   if ARGV.empty?
     puts "#{__FILE__} <base folder>"
     puts "scan all Java cass files recursively and find interface names."
@@ -13,6 +13,6 @@ if __FILE__ == $0
   end
 
   cp = JavaClass.classpath(ARGV[0])
-  puts cp.names.collect { |name| JavaClass.load_cp(name, cp) }.find_all { |clazz| clazz.access_flags.interface? && clazz.access_flags.annotation? }
-
+  puts cp.names.collect { |name| load_cp(name, cp) }.find_all { |clazz| clazz.access_flags.interface? && !clazz.access_flags.annotation? }
+  # TODO check all interfaces how they arenamed,
 end
