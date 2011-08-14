3eafda4bc77d7710f61d89392109e073cdf21e94
Peter Kofler <peter.kofler@code-cop.org>
2011-08-14 11:45:41 +0200

    implement caching

--- lib/javaclass/classpath/caching_classpath.rb
+++ lib/javaclass/classpath/caching_classpath.rb
@@ -1,17 +1,28 @@
 module JavaClass
   module Classpath
 
-    # A delegator that caches returned classes.
+    # A delegator that caches returned class files.
     # Author::          Peter Kofler
     class CachingClasspath
 
       def initialize(delegatee)
         @delegatee = delegatee
+        @cache = {}
+      end
+
+      # Ask the cache for the _classname_ and return it. Else delegate loading to +delegatee+ .
+      def load_binary(classname)
+        key = classname.to_javaname.full_name
+        if !@cache.include?(key)
+          @cache[key] = @delegatee.load_binary(classname)
+        end
+        @cache[key]
       end
 
       alias :__old_method_missing :method_missing
 
-      # Send message to +delegatee+
+      # Delegate all other messages to +delegatee+ . This might not work for methods defined in +Object+ because they are
+      # overwritten here.
       def method_missing(method_id, *args)
         @delegatee.send(method_id, *args)
       end
--- test/test_caching_classpath.rb
+++ test/test_caching_classpath.rb
@@ -1,23 +1,35 @@
 require File.dirname(__FILE__) + '/logging_folder_classpath'
+require File.dirname(__FILE__) + '/test_folder_classpath'
 require 'javaclass/classpath/caching_classpath'
 
 module TestJavaClass
   module TestClasspath
 
-    class TestCachingClasspath < Test::Unit::TestCase
+    class TestCachingClasspath < TestFolderClasspath
+      # extend TestFolderClasspath to execute all tests again
 
-      def test_string
+      def test_delegating
         d = JavaClass::Classpath::CachingClasspath.new(String.new('3'))
         assert_equal(3, d.to_i)
         assert_equal('33', d * 2)
         assert_equal('a', d.sub(/./, 'a'))
       end
 
-      # TODO create cache with folder
-      # load once, was_called is true, reset it
-      # load again, now it's still false
-      
-      # TODO else execute all tests that folderclasspath is supposed to 
+      def setup
+        super
+        @cpe = JavaClass::Classpath::CachingClasspath.new(@cpe)
+      end
+
+      def test_load_binary_cached
+        @cpe.was_called = false
+        @cpe.load_binary('ClassVersionTest10')
+        assert(@cpe.was_called)
+        
+        # load again, now it's still false
+        @cpe.was_called = false
+        @cpe.load_binary('ClassVersionTest10')
+        assert(!@cpe.was_called)
+      end
     end
 
   end
