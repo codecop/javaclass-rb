8dc418e88d37a5a35c1d4ce94978e4f8b6f63a36
Peter Kofler <peter.kofler@code-cop.org>
2011-09-06 09:44:18 +0200

    make DSL a top level require which loads everything needed

--- lib/javaclass/dsl.rb
+++ lib/javaclass/dsl.rb
@@ -1,16 +1,24 @@
+require 'javaclass'
 require 'javaclass/dsl/java_name_factory'
 
 module JavaClass
 
-  # The module Dsl contains shortcuits to make loading and analysing classes
+  # The module DSL contains shortcuits to make loading and analysing classes
   # easier. This ois the high-level API with usage/DSL features.
   # Author::          Peter Kofler
   module Dsl
 
+    # Delegate JavaClass.
+    def classpath(path)
+      JavaClass::classpath(path)
+    end
+    
   end
 
 end
 
 class Object # :nodoc:
+  include JavaClass
+  include JavaClass::Dsl
   include JavaClass::Dsl::JavaNameFactory
 end
