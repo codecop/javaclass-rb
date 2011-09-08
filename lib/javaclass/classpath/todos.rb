ca9b9fc8811c7c844300cef7a64f0fb2d25bd46a
Peter Kofler <peter.kofler@code-cop.org>
2011-09-08 22:43:40 +0200

    add TODOs

---
+++ lib/javaclass/classpath/todos.rb
@@ -0,0 +1,2 @@
+# TODO Single Project Classpath <project name>/[bin oder classes oder target/classes oder target/test-classes]
+# TODO Eclipse Workspace Classpath = workspace/ und Liste von Single Project Classpath 
