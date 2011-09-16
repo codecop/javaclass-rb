2046911e8e32d9e2f141ed1042758795371a7b79
Peter Kofler <peter.kofler@code-cop.org>
2011-09-16 21:18:41 +0200

    remove TODO

--- lib/javaclass/classfile/access_flag_constants.rb
+++ lib/javaclass/classfile/access_flag_constants.rb
@@ -10,7 +10,7 @@ module JavaClass
       ACC_SUPER = 0x0020 # old invokespecial instruction semantics
       ACC_INTERFACE = 0x0200
       ACC_ABSTRACT = 0x0400
-      ACC_INNER = 0x1000 # TODO INNER is an unknown access flag, find in spec
+      ACC_INNER = 0x1000 
       ACC_ANNOTATION = 0x2000
       ACC_ENUM = 0x4000
       # TODO Java 1.0 - "private protected" fields?
