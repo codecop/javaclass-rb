318134981428775610c2876a17bc17d0b56596fc
Peter Kofler <peter.kofler@code-cop.org>
2011-09-16 17:11:02 +0200

    ignore empty classpath elements

--- lib/javaclass/classpath/composite_classpath.rb
+++ lib/javaclass/classpath/composite_classpath.rb
@@ -53,7 +53,7 @@ module JavaClass
 
       # Add the _elem_ classpath element to the list.
       def add_element(elem)
-        @elements << elem unless @elements.find { |cpe| cpe == elem }
+        @elements << elem unless elem.count == 0 || @elements.find { |cpe| cpe == elem } 
         elem.additional_classpath.each do |acpe|
           # referred classpath elements may be missing
           if FileTest.exist? acpe
