9e9ae3555badbeafc81322a44dcd5232f69e138f
Peter Kofler <peter.kofler@code-cop.org>
2011-09-16 21:19:01 +0200

    remove TODO

--- lib/javaclass/classpath/todos.rb
+++ lib/javaclass/classpath/todos.rb
@@ -1,2 +1,1 @@
 # TODO Single Project Classpath <project name>/[bin oder classes oder target/classes oder target/test-classes]
-# TODO Eclipse Workspace Classpath = workspace/ und Liste von Single Project Classpath 
