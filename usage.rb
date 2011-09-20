b4603234bdc843b3467a4adc23f6bb8e5ab4240c
Peter Kofler <peter.kofler@code-cop.org>
2011-09-20 10:19:06 +0200

    resort usage example

--- usage.rb
+++ usage.rb
@@ -10,31 +10,28 @@ if __FILE__ == $0
   
   # or look up the class from some classpath by its Java qualified name
   cp = JavaClass.classpath('./test/data/access_flags')
+  puts cp.includes?('AccessFlagsTestPublic')  # => true
   clazz = JavaClass.load_cp('AccessFlagsTestPublic', cp)
-  
+    
   # retrieve low level information about a class
   puts clazz.version                          # => 50.0
   puts clazz.constant_pool.items[1]           # => packagename/AccessFlagsTestPublic
   puts clazz.access_flags.public?             # => true
+  puts clazz.access_flags.final?              # => false
   puts clazz.this_class                       # => packagename/AccessFlagsTestPublic
-  puts clazz.this_class.to_java_file          # => packagename/AccessFlagsTestPublic.java
   puts clazz.super_class                      # => java/lang/Object
   puts clazz.super_class.to_classname         # => java.lang.Object
   puts clazz.references.referenced_methods[0] # => java/lang/Object.<init>:()V
+  p clazz.interfaces                          # => []
 
-  # work with all class names
+  # work with class names
+  puts clazz.this_class.to_java_file          # => packagename/AccessFlagsTestPublic.java
   puts clazz.this_class.full_name             # => packagename.AccessFlagsTestPublic
   puts clazz.this_class.package               # => packagename
   puts clazz.this_class.simple_name           # => AccessFlagsTestPublic
-  
+
   # or get a class from the system classpath
-  cp = JavaClass.environment_classpath        # needs JAVA_HOME to be set
-  puts cp.includes?('java.lang.String')       # => true
-  data = cp.load_binary(java.lang.String)
-  clazz = JavaClass.disassemble(data)
-  puts clazz.name                             # => "java.lang.String"
-  puts clazz.access_flags.final?              # => true
-  puts clazz.interfaces                       # => ["java/io/Serializable", "java/lang/Comparable", "java/lang/CharSequence"]
+  # cp = JavaClass.environment_classpath        # needs JAVA_HOME to be set
   
 end
 
