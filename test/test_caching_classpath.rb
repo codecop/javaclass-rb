7d9e08a0d00e3d6a7db8afd50730355371cd6ebb
Peter Kofler <peter.kofler@code-cop.org>
2011-09-23 01:24:46 +0200

    change caching classpath for loaded classes

--- test/test_caching_classpath.rb
+++ test/test_caching_classpath.rb
@@ -1,33 +1,38 @@
 require File.dirname(__FILE__) + '/setup'
-require 'javaclass/classpath/caching_classpath'
+require 'javaclass/dsl/caching_classpath'
 require File.dirname(__FILE__) + '/logging_folder_classpath'
 require File.dirname(__FILE__) + '/test_folder_classpath'
 
 module TestJavaClass
-  module TestClasspath
+  module TestDsl
 
-    class TestCachingClasspath < TestFolderClasspath
+    class TestCachingClasspath < TestClasspath::TestFolderClasspath
       # extend TestFolderClasspath to execute all tests again
 
       def test_new_wrong_type
         assert_raise(RuntimeError){ 
-          JavaClass::Classpath::CachingClasspath.new('3') 
+          JavaClass::Dsl::CachingClasspath.new('3') 
         }
       end
 
       def setup
         super
-        @cpe = JavaClass::Classpath::CachingClasspath.new(@cpe)
+        class << @cpe
+          def load(*obj)
+            load_binary(*obj)
+          end
+        end
+        @cpe = JavaClass::Dsl::CachingClasspath.new(@cpe)
       end
 
-      def test_load_binary_cached
+      def test_load_cached
         @cpe.was_called = false
-        @cpe.load_binary('ClassVersionTest10')
+        @cpe.load('ClassVersionTest10')
         assert(@cpe.was_called)
         
         # load again, now it's still false
         @cpe.was_called = false
-        @cpe.load_binary('ClassVersionTest10')
+        @cpe.load('ClassVersionTest10')
         assert(!@cpe.was_called)
       end
     end
