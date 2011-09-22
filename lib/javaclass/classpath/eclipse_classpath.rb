8063cacc769a607d5223955e02caff36a9e00fb4
Peter Kofler <peter.kofler@code-cop.org>
2011-09-22 19:28:18 +0200

    refactor extract constant

--- lib/javaclass/classpath/eclipse_classpath.rb
+++ lib/javaclass/classpath/eclipse_classpath.rb
@@ -6,10 +6,12 @@ module JavaClass
     # An Eclipse workspace aware classpath.
     # Author::   Peter Kofler
     class EclipseClasspath < CompositeClasspath
+      
+      DOT_CLASSPATH = '.classpath'
 
       # Check if the _file_ is a valid location for an Eclipse classpath.
       def self.valid_location(file)
-        FileTest.exist?(file) && FileTest.directory?(file) && FileTest.exist?(File.join(file, '.classpath'))
+        FileTest.exist?(file) && FileTest.directory?(file) && FileTest.exist?(File.join(file, DOT_CLASSPATH))
       end
 
       # Create a classpath for an Eclipse base project in _folder_ where the .classpath is.
@@ -18,7 +20,7 @@ module JavaClass
         super()
         @root = folder
         # kind="output" path="classes"/>
-        classpath = IO.readlines(File.join(@root, '.classpath'))
+        classpath = IO.readlines(File.join(@root, DOT_CLASSPATH))
         classpath.find_all { |line| line =~ /kind\s*=\s*"output"/ }.each do |line|
           if line =~ /path="([^"]+)"/
             add_file_name(File.join(@root, $1))
@@ -32,7 +34,7 @@ module JavaClass
       end
 
       def to_s
-        File.join(@root, '.classpath').to_s
+        File.join(@root, DOT_CLASSPATH).to_s
       end
 
     end
