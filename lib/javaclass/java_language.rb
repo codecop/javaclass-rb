16181c55e30b842a0e4ae854fa744a8a68820802
Peter Kofler <peter.kofler@code-cop.org>
2011-09-25 01:46:42 +0200

    move resources to separate folder

--- lib/javaclass/java_language.rb
+++ lib/javaclass/java_language.rb
@@ -14,12 +14,14 @@ module JavaClass
     # A proper member (field or method) name.
     MEMBER_REGEX = /^[a-z][a-zA-Z0-9_$]*$/
 
+    RESOURCE_FOLDER = File.join(File.dirname(__FILE__), 'resources')
+
     # Reserved words of the Java language.
-    RESERVED_WORDS = IO.readlines(File.dirname(__FILE__) + '/reserved_words.txt').map { |l| l.chomp }
+    RESERVED_WORDS = IO.readlines(File.join(RESOURCE_FOLDER, 'reserved_words.txt')).map { |l| l.chomp }
 
     # List of ISO 3166 two letter country names. Used to recognize valid domain suffix/Java package names.
     # See::            http://www.iso.org/iso/list-en1-semic-3.txt
-    ISO_COUNTRIES = IO.readlines(File.dirname(__FILE__) + '/iso_3166_countries.txt').map { |l| l.chomp }
+    ISO_COUNTRIES = IO.readlines(File.join(RESOURCE_FOLDER, 'iso_3166_countries.txt')).map { |l| l.chomp }
 
     # List of non  ISO 3166 U.S. domain suffix.
     US_DOMAINS = %w| com net biz org |
@@ -28,7 +30,7 @@ module JavaClass
     ALLOWED_PACKAGE_PREFIX = %w| java javax | + US_DOMAINS + ISO_COUNTRIES
 
     # List of all package prefixes found in the JDK (up to 1.6).
-    JDK_PACKAGES = IO.readlines(File.dirname(__FILE__) + '/jdk_packages.txt').map { |l| l.chomp }
+    JDK_PACKAGES = IO.readlines(File.join(RESOURCE_FOLDER, 'jdk_packages.txt')).map { |l| l.chomp }
     JDK_PACKAGES_REGEX = JDK_PACKAGES.collect { |pkg| /^#{pkg}\./ }
 
   end
