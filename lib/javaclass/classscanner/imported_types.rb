0a435fbd46a17500767f0e68487283af4b9aa757
Peter Kofler <peter.kofler@code-cop.org>
2011-09-25 23:25:26 +0200

    comment and add test for 3rd party

--- lib/javaclass/classscanner/imported_types.rb
+++ lib/javaclass/classscanner/imported_types.rb
@@ -1,28 +1,26 @@
 require 'delegate'
-require 'javaclass/classfile/java_class_header'
 
 module JavaClass
-
   module ClassScanner
 
     # Add analysis for imported types to +JavaClassHeader+.
     # Author::          Peter Kofler
-    class ImportedTypes < DelegateClass(JavaClass::ClassFile::JavaClassHeader)
+    class ImportedTypes < SimpleDelegator
 
-      # Decorate JavaClassHeader _header_ .
+      # Decorate JavaClassHeader _header_ to add imported types lazy scanner.
       def initialize(header)
         super(header)
         @imported_types = nil
       end
 
-      # Determine the imported types of this class and return their names.
+      # Determine the imported types of this class and return their names. This does not contain the name if this class itself.
       def imported_types
         @imported_types = @imported_types ||
           references.used_classes.collect { |c| c.to_s.full_name }.sort
         @imported_types
       end
 
-      # Determine the imported types of this class which are not from the JDK.
+      # Determine the imported types of this class which are not from the JDK. This are all imported_types - all jdk types.
       def imported_3rd_party_types
         imported_types.reject { |name| name.in_jdk? }
       end
