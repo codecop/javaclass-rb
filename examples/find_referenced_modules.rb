0e9584720c63cc50a6af5a52bf33ed2d2b16587c
Peter Kofler <peter.kofler@code-cop.org>
2011-09-30 19:11:08 +0200

    implement find unreferenced modules

--- examples/find_referenced_modules.rb
+++ examples/find_referenced_modules.rb
@@ -1,7 +1,32 @@
-# TODO CONTINUE 8 - find_referenced_modules.rb example/tool for SDM
+# Example usage of classpath and class files: Scan all classes of an Eclipse
+# "workspace". Use the classes of one module and find all modules which are not
+# referenced. This lists unused libraries (classpaths).
+# Author::          Peter Kofler
 
-# create a whole classpath with all elements from a workspace
-# for a specific or all modules do
-# reset accessed
-# for all classes in this specific module get referenced classes and mark them
-# find all classpath elements that have been marked 
+# add the lib of this gem to the load path
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'javaclass/classpath/tracking_classpath'
+require 'javaclass/dsl/mixin'
+
+workspace_location = 'C:\Users\IBM_ADMIN\Dropbox\InArbeit\DRY_Research\In the Wild\Java5_Servlets(HBD)'
+package = at.herold.*
+
+# 1) create the CompositeClasspath of the given workspace
+cp = workspace(workspace_location)
+puts "#{ws.elements.size} classpaths found under the workspace #{workspace_location}:\n  #{ws.elements.join("\n  ")}"
+
+# 2) create another classpath of the module under test and load all classes 
+classes = classpath(File.join(workspace_location, 'classes')).values
+puts "#{classes.size} classes loaded from classpaths"
+
+# 3) mark all their referenced types as accessed in the workspace
+cp.reset_access
+classes.map { |clazz| clazz.imported_types }.flatten.
+   each { |clazz| cp.mark_accessed(clazz) }
+puts 'all classes mapped'
+
+# 4) now find non accessed modules (i.e. classpath elements)
+unused = cp.elements.find_all { |clp| !clp.accessed? }
+puts "#{unused.size} unused modules found:\n  #{unused.join("\n  ")}"
+
+p cp.elements.map { |clp| [clp.to_s, clp.accessed?] }
