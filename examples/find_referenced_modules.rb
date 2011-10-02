bd9adb2bde6efeb753a6006e8cf6634ca42686e4
Peter Kofler <peter.kofler@code-cop.org>
2011-10-02 23:50:09 +0200

    test find unreferenced modules...

--- examples/find_referenced_modules.rb
+++ examples/find_referenced_modules.rb
@@ -5,25 +5,24 @@
 
 # add the lib of this gem to the load path
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-require 'javaclass/classpath/tracking_classpath'
+require 'javaclass/classpath/tracking_classpath' # load tracking first!
 require 'javaclass/dsl/mixin'
+# require 'profile'
 
-workspace_location = 'C:\Users\IBM_ADMIN\Dropbox\InArbeit\DRY_Research\In the Wild\Java5_Servlets(HBD)'
-package = at.herold.*
+workspace_location = 'C:\Users\pkofler\Dropbox\InArbeit\DRY_Research\In the Wild\Java5_Servlets(HBD)'
 
 # 1) create the CompositeClasspath of the given workspace
 cp = workspace(workspace_location)
-puts "#{ws.elements.size} classpaths found under the workspace #{workspace_location}:\n  #{ws.elements.join("\n  ")}"
+puts "#{cp.elements.size} classpaths found under the workspace #{workspace_location}:\n  #{cp.elements.join("\n  ")}"
 
-# 2) create another classpath of the module under test and load all classes 
-classes = classpath(File.join(workspace_location, 'classes')).values
-puts "#{classes.size} classes loaded from classpaths"
+# 2) create another classpath of the module under test 
+main_classes = classpath(File.join(workspace_location, 'classes'), classpath(File.join(workspace_location, 'test-classes')))
+puts "#{main_classes.names.size} classes found in main classpath"
 
 # 3) mark all their referenced types as accessed in the workspace
 cp.reset_access
-classes.map { |clazz| clazz.imported_types }.flatten.
-   each { |clazz| cp.mark_accessed(clazz) }
-puts 'all classes mapped'
+main_classes.external_types.each { |clazz| cp.mark_accessed(clazz) }
+puts 'all referenced classes mapped'
 
 # 4) now find non accessed modules (i.e. classpath elements)
 unused = cp.elements.find_all { |clp| !clp.accessed? }
