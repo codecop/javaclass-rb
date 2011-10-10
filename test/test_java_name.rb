187ca295de6abc85ea3ebe5a7d0603f46f2b92e1
Peter Kofler <peter.kofler@code-cop.org>
2011-10-10 19:02:13 +0200

    add test for JavaName, example is still failing

--- test/test_java_name.rb
+++ test/test_java_name.rb
@@ -15,10 +15,125 @@ end
 
 module TestJavaClass
 
-  class TestJavaName < Test::Unit::TestCase
+  class TestPackageLogic < Test::Unit::TestCase
+    include JavaClass::PackageLogic
+
+    def setup
+      @package = 'at.kugel.tool.'
+    end
+        
+    def test_same_or_subpackage_of_eh
+      assert(same_or_subpackage_of?(%w[ at.kugel ]))
+      assert(same_or_subpackage_of?(%w[ at.kugel.tool ]))
+      assert(!same_or_subpackage_of?(%w[ at.kugel.tool.some ]))
+      assert(same_or_subpackage_of?(%w[ java at.kugel ]))
+
+      @package = ''
+      assert(!same_or_subpackage_of?(%w[ at at.kugel ]))
+    end
+
+    def test_subpackage_of_eh
+      assert(subpackage_of?(%w[ at.kugel ]))
+      assert(subpackage_of?(%w[ at.kugel.tool ]))
+      assert(!subpackage_of?(%w[ at.kugel.tool.class ]))
+    end
+
+    def test_in_jdk_eh
+      assert(!in_jdk?)
+
+      @package = 'java.lang'
+      assert(in_jdk?)
+    end
+        
+  end
+  
+  class TestSimpleNameLogic < Test::Unit::TestCase
+    include JavaClass::SimpleNameLogic
+
+    def setup
+      @simple_name = 'SomeClassWithMoreNames'
+    end
+
+    def test_split_simple_name
+      assert_equal(['', 'SomeClassWithMoreNames'], split_simple_name(0))
+      assert_equal(['Some', 'ClassWithMoreNames'], split_simple_name(1))
+      assert_equal(['SomeClass', 'WithMoreNames'], split_simple_name(2))
+      assert_equal(['SomeClassWith', 'MoreNames'], split_simple_name(3))
+      assert_equal(['SomeClassWithMore', 'Names'], split_simple_name(4))
+      assert_equal(['SomeClassWithMoreNames', ''], split_simple_name(5))
+      assert_equal(['SomeClassWithMoreNames', ''], split_simple_name(6))
+
+      assert_equal(['SomeClassWithMoreNames', ''], split_simple_name(-1))
+      assert_equal(['SomeClassWithMore', 'Names'], split_simple_name(-2))
+      assert_equal(['SomeClassWith', 'MoreNames'], split_simple_name(-3))
+      assert_equal(['SomeClass', 'WithMoreNames'], split_simple_name(-4))
+      assert_equal(['Some', 'ClassWithMoreNames'], split_simple_name(-5))
+      assert_equal(['', 'SomeClassWithMoreNames'], split_simple_name(-6))
+      assert_equal(['', 'SomeClassWithMoreNames'], split_simple_name(-7))
+    end
+  
+  end
+  
+  class TestJavaQualifiedName < Test::Unit::TestCase
 
     def setup
       @simple_name = JavaClass::JavaQualifiedName.new('Some')
+      @qualified_name= JavaClass::JavaQualifiedName.new('at.kugel.tool.Some')
+    end
+
+    def test_class_new_invalid
+      assert_raise(ArgumentError) { JavaClass::JavaQualifiedName.new('at.kugel.tool/Some') }
+      assert_raise(ArgumentError) { JavaClass::JavaQualifiedName.new('at.kugel.tool.') }
+    end
+
+    def test_class_new_caches
+      cn = JavaClass::JavaQualifiedName.new('at.kugel.tool.Some', 1, 2 )
+      assert_equal(1, cn.to_jvmname)
+      assert_equal(2, cn.to_class_file)
+    end
+    
+    def test_package
+      assert_equal('', @simple_name.package)
+      assert_equal('at.kugel.tool', @qualified_name.package)
+    end
+
+    def test_simple_name
+      assert_equal('Some', @simple_name.simple_name)
+      assert_equal('Some', @qualified_name.simple_name)
+    end
+
+    def test_full_name
+      assert_equal('Some', @simple_name.full_name)
+      assert_equal('at.kugel.tool.Some', @qualified_name.full_name)
+    end
+
+    def test_to_javaname
+      assert_same(@qualified_name, @qualified_name.to_javaname)
+    end
+
+    def test_to_classname
+      assert_same(@qualified_name, @qualified_name.to_classname)
+    end
+
+    def test_to_jvmname
+      assert_equal('at/kugel/tool/Some', @qualified_name.to_jvmname)
+      assert_same(@qualified_name.to_jvmname, @qualified_name.to_jvmname) # is saved
+    end
+  
+    def test_to_java_file
+      assert_equal('at/kugel/tool/Some.java', @qualified_name.to_java_file)
+    end
+
+    def test_to_class_file
+      assert_equal('at/kugel/tool/Some.class', @qualified_name.to_class_file)
+      assert_same(@qualified_name.to_class_file, @qualified_name.to_class_file) # is saved
+    end
+    
+  end
+
+  class TestJavaName < Test::Unit::TestCase
+
+    def setup
       #@source_file = JavaClass::JavaName.new("at\\kugel\\tool\\Some.java")
       @jvm_path = JavaClass::JavaVMName.new('at/kugel/tool/SomeClassWithMoreNames')
       #@jvm_method_name = JavaClass::JavaName.new('at/kugel/tool/Some.<init>')
@@ -28,7 +143,6 @@ module TestJavaClass
     end
 
     def test_to_classname
-      assert_equal('Some', @simple_name.to_classname)
       #assert_equal('at.kugel.tool.Some', @source_file.to_classname)
       assert_equal('at.kugel.tool.SomeClassWithMoreNames', @jvm_path.to_classname)
       #assert_equal('at.kugel.tool.Some', @jvm_method_name.to_classname)
@@ -43,7 +157,6 @@ module TestJavaClass
     end
 
     def test_to_jvmname
-      assert_equal('Some', @simple_name.to_jvmname)
       #assert_equal('at/kugel/tool/Some', @source_file.to_jvmname)
       assert_equal('at/kugel/tool/SomeClassWithMoreNames', @jvm_path.to_jvmname)
       #assert_equal('at/kugel/tool/Some', @jvm_method_name.to_jvmname)
@@ -51,7 +164,6 @@ module TestJavaClass
     end
 
     def test_to_java_file
-      assert_equal('Some.java', @simple_name.to_java_file)
       #assert_equal('at/kugel/tool/Some.java', @source_file.to_java_file)
       assert_equal('at/kugel/tool/SomeClassWithMoreNames.java', @jvm_path.to_java_file)
       #assert_equal('at/kugel/tool/Some.java', @jvm_method_name.to_java_file)
@@ -59,7 +171,6 @@ module TestJavaClass
     end
 
     def test_to_class_file
-      assert_equal('Some.class', @simple_name.to_class_file)
       #assert_equal('at/kugel/tool/Some.class', @source_file.to_class_file)
       assert_equal('at/kugel/tool/SomeClassWithMoreNames.class', @jvm_path.to_class_file)
       #assert_equal('at/kugel/tool/Some.class', @jvm_method_name.to_class_file)
@@ -67,7 +178,6 @@ module TestJavaClass
     end
 
     def test_full_name
-      assert_equal('Some', @simple_name.full_name)
       #assert_equal('at.kugel.tool.Some', @source_file.full_name)
       assert_equal('at.kugel.tool.SomeClassWithMoreNames', @jvm_path.full_name)
       #assert_equal('at.kugel.tool.Some', @jvm_method_name.full_name)
@@ -75,54 +185,15 @@ module TestJavaClass
     end
 
     def test_package
-      assert_equal('', @simple_name.package)
       #assert_equal('at.kugel.tool', @source_file.package)
       assert_equal('at.kugel.tool', @jvm_path.package)
     end
 
     def test_simple_name
-      assert_equal('Some', @simple_name.simple_name)
       #assert_equal('Some', @source_file.simple_name)
       #assert_equal('SomeClassWithMoreNames', @jvm_path.simple_name)
     end
 
-    def test_same_or_subpackage_of_eh
-      #assert(@source_file.same_or_subpackage_of?(%w[ at.kugel ]))
-      #assert(@source_file.same_or_subpackage_of?(%w[ at.kugel.tool ]))
-      #assert(!@source_file.same_or_subpackage_of?(%w[ at.kugel.tool.some ]))
-      #assert(@source_file.same_or_subpackage_of?(%w[ java at.kugel ]))
-
-      assert(!@simple_name.same_or_subpackage_of?(%w[ at at.kugel ]))
-    end
-
-    def test_subpackage_of_eh
-      #assert(@source_file.subpackage_of?(%w[ at.kugel ]))
-      #assert(!@source_file.subpackage_of?(%w[ at.kugel.tool ]))
-      #assert(!@source_file.subpackage_of?(%w[ at.kugel.tool.some ]))
-    end
-
-    def test_split_simple_name
-      assert_equal(['', 'SomeClassWithMoreNames'], @jvm_path.split_simple_name(0))
-      assert_equal(['Some', 'ClassWithMoreNames'], @jvm_path.split_simple_name(1))
-      assert_equal(['SomeClass', 'WithMoreNames'], @jvm_path.split_simple_name(2))
-      assert_equal(['SomeClassWith', 'MoreNames'], @jvm_path.split_simple_name(3))
-      assert_equal(['SomeClassWithMore', 'Names'], @jvm_path.split_simple_name(4))
-      assert_equal(['SomeClassWithMoreNames', ''], @jvm_path.split_simple_name(5))
-      assert_equal(['SomeClassWithMoreNames', ''], @jvm_path.split_simple_name(6))
-
-      assert_equal(['SomeClassWithMoreNames', ''], @jvm_path.split_simple_name(-1))
-      assert_equal(['SomeClassWithMore', 'Names'], @jvm_path.split_simple_name(-2))
-      assert_equal(['SomeClassWith', 'MoreNames'], @jvm_path.split_simple_name(-3))
-      assert_equal(['SomeClass', 'WithMoreNames'], @jvm_path.split_simple_name(-4))
-      assert_equal(['Some', 'ClassWithMoreNames'], @jvm_path.split_simple_name(-5))
-      assert_equal(['', 'SomeClassWithMoreNames'], @jvm_path.split_simple_name(-6))
-      assert_equal(['', 'SomeClassWithMoreNames'], @jvm_path.split_simple_name(-7))
-    end
-
-    def test_in_jdk_eh
-      #assert(!@source_file.in_jdk?)
-      assert(@jdk_class_file.in_jdk?)
-    end
   end
 
 end
