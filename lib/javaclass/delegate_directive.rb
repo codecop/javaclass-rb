2e479f135f3efde266ffef8a9a42608a5c7af1b0
Peter Kofler <peter.kofler@code-cop.org>
2011-10-11 16:56:18 +0200

    add delegation to a field

--- lib/javaclass/delegate_directive.rb
+++ lib/javaclass/delegate_directive.rb
@@ -1,10 +1,15 @@
 # Delegation directives.
 # Author::          Peter Kofler
 module DelegateDirective # :nodoc:
-
-  # Directive to create a simple delegating method to _delegate_ with _method_name_ .
+  
+  # Directive to create a delegating method _method_name_ to the result of my own method _delegate_ without arguments.
   def delegate(method_name, delegate)
     self.module_eval("def #{method_name}(*obj) #{delegate}.#{method_name}(*obj) end")
+  end
+
+  # Directive to create a delegating method _method_name_ to the field _field_name_ .
+  def delegate_field(method_name, field_name)
+    self.module_eval("def #{method_name}(*obj) @#{field_name}.#{method_name}(*obj) end")
   end
 
 end
