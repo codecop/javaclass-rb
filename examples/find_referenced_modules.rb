c0831f8d27e10b2237a77a37f1716748655be1cc
Peter Kofler <peter.kofler@code-cop.org>
2011-10-12 00:13:26 +0200

    finish script, works :-)

--- examples/find_referenced_modules.rb
+++ examples/find_referenced_modules.rb
@@ -5,27 +5,30 @@
 
 # add the lib of this gem to the load path
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-require 'javaclass/classpath/tracking_classpath' # load tracking first!
 require 'javaclass/dsl/mixin'
 
 workspace_location = "#{ENV['USERPROFILE']}\\Dropbox\\InArbeit\\Corpus\\Java6_Web(HBD)"
-workspace_location = "#{ENV['USERPROFILE']}\\Dropbox\\InArbeit\\Corpus\\Java6_Swing(BIA)"
+#workspace_location = "#{ENV['USERPROFILE']}\\Dropbox\\InArbeit\\Corpus\\Java6_Swing(BIA)"
 
-# 1) create the CompositeClasspath of the given workspace
+# 1) create a classpath of the module(s) under test 
+main_classes = classpath(File.join(workspace_location, 'classes'), classpath(File.join(workspace_location, 'test-classes')))
+puts "#{main_classes.names.size} classes found in main classpath:\n  #{main_classes.elements.join("\n  ")}"
+
+# 2) load tracking before creating new classpaths
+require 'javaclass/classpath/tracking_classpath' 
+
+# 3) create the (tracking) CompositeClasspath of the given workspace
 cp = workspace(workspace_location)
 puts "#{cp.elements.size} classpaths found under the workspace #{workspace_location}:\n  #{cp.elements.join("\n  ")}"
 
-# 2) create another classpath of the module under test 
-main_classes = classpath(File.join(workspace_location, 'classes'), classpath(File.join(workspace_location, 'test-classes')))
-puts "#{main_classes.names.size} classes found in main classpath"
-
-# 3) mark all their referenced types as accessed in the workspace
+# 4) mark all their referenced types as accessed in the workspace
+puts 'mapping referenced classes... (can take several minutes)'
 cp.reset_access
 main_classes.external_types.each { |clazz| cp.mark_accessed(clazz) }
 puts 'all referenced classes mapped'
 
-# 4) now find non accessed modules (i.e. classpath elements)
-unused = cp.elements.find_all { |clp| !clp.accessed? }
+# 5) now find non accessed modules (i.e. classpath elements)
+unused = cp.elements.find_all { |clp| !clp.accessed? && clp.jar? }
 puts "#{unused.size} unused modules found:\n  #{unused.join("\n  ")}"
 
-p cp.elements.map { |clp| [clp.to_s, clp.accessed?] }
+# puts cp.elements.map { |clp| "#{clp.to_s}: #{clp.accessed?}" }
