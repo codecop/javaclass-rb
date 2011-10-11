3d6e28ace52150d5beb2669e273c53bdb6dc493c
Peter Kofler <peter.kofler@code-cop.org>
2011-10-11 23:19:20 +0200

    doc

--- examples/simple_usage.rb
+++ examples/simple_usage.rb
@@ -18,21 +18,21 @@ puts cp.includes?('AccessFlagsTestPublic')  # => 1 (true)
 clazz = JavaClass.load_cp('AccessFlagsTestPublic', cp)
   
 # 2) retrieve low level information about a class
-puts clazz.version                          # => 50.0
-puts clazz.constant_pool.items[1]           # => packagename/AccessFlagsTestPublic
+puts clazz.version                          # => "50.0"
+puts clazz.constant_pool.items[1]           # => "packagename/AccessFlagsTestPublic"
 puts clazz.access_flags.public?             # => true
 puts clazz.access_flags.final?              # => false
-puts clazz.this_class                       # => packagename/AccessFlagsTestPublic
-puts clazz.super_class                      # => java/lang/Object
-puts clazz.super_class.to_classname         # => java.lang.Object
-puts clazz.references.referenced_methods[0] # => java/lang/Object.<init>:()V
+puts clazz.this_class                       # => "packagename/AccessFlagsTestPublic"
+puts clazz.super_class                      # => "java/lang/Object"
+puts clazz.super_class.to_classname         # => "java.lang.Object"
+puts clazz.references.referenced_methods[0] # => "java/lang/Object.<init>:()V"
 puts clazz.interfaces                       # => []
 
 # 3) work with class names
-puts clazz.this_class.to_java_file          # => packagename/AccessFlagsTestPublic.java
-puts clazz.this_class.full_name             # => packagename.AccessFlagsTestPublic
-puts clazz.this_class.package               # => packagename
-puts clazz.this_class.simple_name           # => AccessFlagsTestPublic
+puts clazz.this_class.to_java_file          # => "packagename/AccessFlagsTestPublic.java"
+puts clazz.this_class.full_name             # => "packagename.AccessFlagsTestPublic"
+puts clazz.this_class.package               # => "packagename"
+puts clazz.this_class.simple_name           # => "AccessFlagsTestPublic"
 
 # TODO CONTINUE 5 - create a script that creates the *.txt for the good examples from rb and includes them in rdoc 
 # (this is in fact a rake task)
