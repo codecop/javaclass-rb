df44e729e5de0dbb2e4294b0c8df66a8ad08f4b9
Peter Kofler <peter.kofler@code-cop.org>
2011-10-12 20:59:04 +0200

    fix wrong boolean

--- lib/javaclass/java_name_scanner.rb
+++ lib/javaclass/java_name_scanner.rb
@@ -9,8 +9,12 @@ module JavaClass
     # Find all possible class names in all XML and property files under _path_
     def scan_config_for_class_names(path)
       return unless File.exist? path
-      if File.file?(path) && path =~ /\.xml$|\.properties$|MANIFEST.MF$/
-        scan_text_for_class_names(IO.readlines(path).join)
+      if File.file?(path) 
+        if path =~ /\.xml$|\.properties$|MANIFEST.MF$/
+          scan_text_for_class_names(IO.readlines(path).join)
+        else
+          []
+        end
       else
         Dir.entries(path).reject { |n| n=~/^\./ }.map { |n| scan_config_for_class_names(File.join(path, n)) }.flatten.sort
       end
