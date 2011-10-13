3b9d7317cbd1af18c229101f2951aa310b3d8cb9
Peter Kofler <peter.kofler@code-cop.org>
2011-10-13 19:00:55 +0200

    temporary commit, code commented out

--- lib/javaclass/java_name_scanner.rb
+++ lib/javaclass/java_name_scanner.rb
@@ -25,7 +25,7 @@ module JavaClass
       \s*
       (   (?:#{JavaLanguage::IDENTIFIER_REGEX}#{JavaLanguage::SEPARATOR_REGEX})+#{JavaLanguage::TYPE_REGEX}   )
       \s*
-      (?:$|<|"|')
+      (?:$|<|"|'|:)
     /x
 
     # Extract the list of possible class names from the given _string_ . This will only find
@@ -34,7 +34,54 @@ module JavaClass
       string.scan(TEXT_REGEX).flatten.map { |match| JavaQualifiedName.new(match) }
     end
 
-    # TODO add method to scan Java String constants of classes to find Class.Forname 
   end
+
+  module ClassScanner
+
+    class ImportedTypes < SimpleDelegator
+
+#      # Determine the imported types of this class and return their names. This does not contain the name if this class itself.
+#      def imported_types
+#        @imported_types ||= references.used_classes.collect { |c| c.class_name.to_classname }.sort
+#      end
+#
+#      # Determine the imported types of this class which are not from the JDK. This are all imported_types - all jdk types.
+#      def imported_3rd_party_types
+#        imported_types.reject { |name| name.in_jdk? }
+#      end
+
+    end
+
+  end
+  
+  module Analyse
+    module Dependencies
+
+      # TODO add method to scan Java String constants of classes to find Class.Forname 
+
+#      # Determine all imported types from all classes in this classpath together with count of imports.
+#      # An additional block is used as _filter_ on class names.
+#      def hardcoded_types(&filter)
+#        type_map = Hash.new(0) # class_name (JavaQualifiedName) => cnt
+#        values(&filter).collect { |clazz| clazz.imported_3rd_party_types }.flatten.each do |type|
+#
+#          # hash keys need to be frozen to keep state
+#          if !type_map.include?(type)
+#            type = type.freeze 
+#          end
+#
+#          type_map[type] += 1
+#        end
+#        type_map
+#      end
+#
+#      # Determine all foreign imported types from all classes in this classpath.
+#      # An additional block is used as _filter_ on class names.
+#      def external_types(&filter)
+#        used_types(&filter).keys.sort - types(&filter)
+#      end
+
+    end
+  end  
 
 end
