a3a36170c62cb32a04f0487c53b39f3c5f138683
Peter Kofler <peter.kofler@code-cop.org>
2011-10-19 00:26:33 +0200

    make nice result display

--- examples/find_referenced_modules.rb
+++ examples/find_referenced_modules.rb
@@ -1,6 +1,8 @@
 # Example usage of classpath and class files: Scan all classes of an Eclipse
 # "workspace". Use the classes of one module and find all modules which are not
-# referenced. This lists unused libraries (classpaths).
+# referenced. This lists *potential* unused libraries (classpaths). Note that the
+# libraries may still be used from configuration files (e.g. Spring contexts) or
+# from used libraries.
 # Author::          Peter Kofler
 
 # add the lib of this gem to the load path
@@ -25,10 +27,15 @@ puts "#{cp.elements.size} classpaths found under the workspace #{workspace_locat
 puts 'mapping referenced classes... (can take several minutes)'
 cp.reset_access
 main_classes.external_types.each { |clazz| cp.mark_accessed(clazz) }
-puts 'all referenced classes mapped'
+puts 'referenced classes mapped'
 
-# 5) now find non accessed modules (i.e. classpath elements)
-unused = cp.elements.find_all { |clp| !clp.accessed? && clp.jar? }
-puts "#{unused.size} unused modules found:\n  #{unused.join("\n  ")}"
+# 5a) now find non accessed modules (i.e. classpath elements)
+unused = cp.elements.find_all { |clp| clp.jar? && clp.accessed? == 0 }
+puts "\n#{unused.size} unused modules found:\n  #{unused.join("\n  ")}"
 
-# puts cp.elements.map { |clp| "#{clp.to_s}: #{clp.accessed?}" }
+# 5b) or print the list of classpath elements with access
+puts "\nlibrary (module path): number of accessed classes"
+puts cp.elements.map { |clp| [clp.to_s, clp.accessed?] }.sort { |a,b| a[1] <=> b[1] }.map { |e| "#{e[0]}: #{e[1]}" }
+
+# TODO CONTINUE 6 - improve loading performance, why does it take so long to load 10.000 headers?
+# use -r profile
