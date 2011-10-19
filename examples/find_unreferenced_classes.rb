945c98774fe5e7dcdbb4da1f52636abda0a8fbc5
Peter Kofler <peter.kofler@code-cop.org>
2011-10-19 11:51:34 +0200

    nicen up example

--- examples/find_unreferenced_classes.rb
+++ examples/find_unreferenced_classes.rb
@@ -1,6 +1,7 @@
 # Example usage of classpath and class files: Scan all classes of an Eclipse
-# "workspace". (A workspace is a folder containing several Eclipse projects.)
-# Find all interfaces, print their names and find all which are prefixed with I.
+# "workspace". Mark all referenced classes. Report unreferenced classes. This 
+# lists *potential* unused classes. Note that the classes may still be used by
+# reflection.
 # Author::          Peter Kofler
 
 # add the lib of this gem to the load path
@@ -8,7 +9,7 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'javaclass/classpath/tracking_classpath'
 require 'javaclass/dsl/mixin'
 
-workspace_location = 'C:\RTC3.0\workspaces\Costing_Dev'
+location = 'C:\RTC3.0\workspaces\Costing_Dev'
 package1 = 'com.ibm.arc.sdm'
 package2 = 'pricingTool'
 
@@ -16,31 +17,40 @@ package2 = 'pricingTool'
 Eclipse.skip_lib
 
 # 1) create the (tracking) CompositeClasspath of the given workspace
-cp = workspace(workspace_location)
-puts "#{cp.elements.size} classpaths found under the workspace #{workspace_location}:\n  #{cp.elements.join("\n  ")}"
+cp = workspace(location)
+puts "#{cp.elements.size} classpaths found under the workspace #{location}:\n  #{cp.elements.join("\n  ")}"
 
+# limit all operations to the classes we are interrested in
 filter = Proc.new { |clazz| clazz.same_or_subpackage_of?(package1) || clazz.same_or_subpackage_of?(package2) }
 
 # 2) load all classes in the given packages
 puts 'loading all *filtered* classes... (can take several minutes)'
 classes = cp.values(&filter)
 puts "#{classes.size} classes loaded from classpaths"
-# TODO CONTINUE 6 - improve loading performance, why does it take so long to load 10.000 headers?
-# use -r profile
 
 # 3) mark all their referenced types in the given packages as accessed
 cp.reset_access
 classes.map { |clazz| clazz.imported_types }.flatten.
     find_all(&filter).
-    each { |clazz| cp.mark_accessed(clazz) }
-    puts 'all *filtered* classes mapped'
+    each { |classname| cp.mark_accessed(classname) }
+puts '*filtered* classes mapped'
 
 # 4) also mark all classes referenced from config files
-hardcoded_classes = scan_config_for_class_names(workspace_location).find_all(&filter)
-puts "#{hardcoded_classes.size} classes references found in configs"
-hardcoded_classes.each { |clazz| cp.mark_accessed(clazz) }
-puts 'all hardcoded classes mapped'
+hardcoded_classnames = scan_config_for_3rd_party_class_names(location).find_all(&filter)
+puts "#{hardcoded_classnames.size} classes references found in configs"
+hardcoded_classnames.each { |classname| cp.mark_accessed(classname) }
+puts 'hardcoded classes mapped'
 
-# 5) find non accessed classes
-unused = cp.names(&filter).find_all { |clazz| !cp.accessed(clazz) }
-puts "#{unused.size} unused classes found:\n  #{unused.join("\n  ")}"
+# 5) mark unit tests (all classes ending in Test on the test projects)
+test_projects = cp.elements.find_all { |cpe| cpe.to_s =~ /\.test\// }
+test_projects.each do |project|
+  project.names { |classname| classname =~ /Test(?:Case|Suite|s)?\.class/ }.
+      each { |classname| project.mark_accessed(classname) }
+end
+puts 'test classes mapped'
+
+# 6) find non accessed classes and report unused in specific packages
+unused_classes = classes.find_all { |clazz| cp.accessed(clazz) == 0 }
+report = unused_classes.reject{ |clazz| clazz.this_class =~ /\/gpe\/|\/s2\// }.
+                        map { |clazz| "#{clazz.to_classname}" }
+puts "#{report.size} unused classes found:\n  #{report.join("\n  ")}"
