0d568418ae61e5547515a059942bd90f2eaa8755
Peter Kofler <peter.kofler@code-cop.org>
2011-10-28 01:05:42 +0200

    add ruby-prof profiling

--- examples/profiler_scratchpad.rb
+++ examples/profiler_scratchpad.rb
@@ -1,5 +1,6 @@
 # Scratchpad script for profiler execution.
 # Author::          Peter Kofler
+require 'ruby-prof'
 
 # add the lib of this gem to the load path
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
@@ -12,12 +13,17 @@ cp = classpath(location)
 puts "#{cp.elements.size} classpaths found under the workspace #{location}:\n  #{cp.elements.join("\n  ")}"
 puts "#{cp.count} classes found in classpaths"
 
-require 'profiler'
-
+RubyProf.start
 # 2) load all classes
 cp.values
+result = RubyProf.stop
 
 # 3) iterate all constant pool entries
 # workspace.external_types
 
 # TODO CONTINUE 6 - improve loading performance, why does it take so long to load 10.000 headers?
+
+# Print a flat profile to text
+printer = RubyProf::GraphPrinter.new(result)
+# printer = RubyProf::FlatPrinter.new(result)
+printer.print(STDOUT)
--- gems/install_original_gems.bat
+++ gems/install_original_gems.bat
@@ -2,3 +2,4 @@ call gem install rake --version 0.8.4
 call gem install rubyzip --version 0.9.1
 call gem install ZenTest --version 4.4.0
 rem call gem install rcov --version 0.9.11
+call gem install ruby-prof -v 0.8.1 x86-mingw32
