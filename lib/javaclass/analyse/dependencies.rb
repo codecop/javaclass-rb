1a867f5253ff007e9af462d7b35f09c43f050251
Peter Kofler <peter.kofler@code-cop.org>
2011-11-13 21:00:47 +0100

    specify needed reuqirements on mixin

--- lib/javaclass/analyse/dependencies.rb
+++ lib/javaclass/analyse/dependencies.rb
@@ -10,13 +10,13 @@ module JavaClass
     module Dependencies
 
       # Return all types in this classpath. An additional block is used as _filter_ on class names.
-      # Returns a list of JavaQualifiedName.
+      # Returns a list of JavaQualifiedName. Requires a method _names_ in the base class.
       def types(&filter)
         names(&filter).collect { |c| c.to_classname }.sort
       end
 
       # Determine all imported types from all classes in this classpath together with count of imports.
-      # An additional block is used as _filter_ on class names.
+      # An additional block is used as _filter_ on class names. Requires a method _values_ in the base class.
       def used_types_map(&filter)
         type_map = Hash.new(0) # class_name (JavaQualifiedName) => cnt
         values(&filter).collect { |clazz| clazz.imported_3rd_party_types }.flatten.each do |type|
