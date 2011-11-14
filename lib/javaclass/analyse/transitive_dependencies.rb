d4e5c6b3b8adf96532fefae26f8e8933a5cfd7a9
Peter Kofler <peter.kofler@code-cop.org>
2011-11-14 01:23:26 +0100

    add filter for class selection in transitivity work

--- lib/javaclass/analyse/transitive_dependencies.rb
+++ lib/javaclass/analyse/transitive_dependencies.rb
@@ -7,35 +7,38 @@ module JavaClass
     # Author::          Peter Kofler
     module TransitiveDependencies
 
-      # Creates the tree of all transitive dependencies where every dependency is only listed once 
-      # (it's first occurence). It returns an AdderTree containing all found dependencies.
+      # Creates the tree of all transitive dependencies of _classname_ where every dependency is only 
+      # listed once at its first occurence. It returns an AdderTree containing all found dependencies.
+      # An additional block is used as _filter_ on class names to work with, e.g. the main classes.
       # Requires methods _includes?_ and _load_ in the base class.
-      def transitive_dependency_tree(classname, tree=nil)
+      def transitive_dependency_tree(classname, tree=nil, &filter)
+        return if block_given? && !filter.call(classname) 
         return if tree && tree.root.contain?(classname)
         
         if tree
-          tree = tree.add(classname)
+          tree = tree.add(classname.to_classname)
         else
-          tree = AdderTree.new(classname)
+          tree = AdderTree.new(classname.to_classname)
         end
 
         if includes?(classname)
           load(classname).imported_3rd_party_types.each do |classname|
-            transitive_dependency_tree(classname, tree)
+            transitive_dependency_tree(classname, tree, &filter)
           end
         end
         
         tree
       end
 
-      # Creates the tree of all transitive dependencies of a Java package where every dependency is only 
-      # listed once (it's first occurence). It returns an AdderTree containing all found dependencies.
+      # Creates the tree of all transitive dependencies of the Java _package_ where every dependency is only 
+      # listed once at its first occurence. It returns an AdderTree containing all found dependencies.
+      # An additional block is used as _filter_ on class names to work with, e.g. the main classes.
       # Requires a method _names_ in the base class.
-      def transitive_dependencies_package(base_package)
-        tree = AdderTree.new(base_package)
+      def transitive_dependencies_package(package, &filter)
+        tree = AdderTree.new(package)
 
-        names { |classname| classname.same_or_subpackage_of?(base_package) }.each do 
-          transitive_dependency_tree(classname, tree)
+        names { |classname| classname.same_or_subpackage_of?(base_package) }.each do |classname| 
+          transitive_dependency_tree(classname, tree, &filter)
         end
         
         tree
