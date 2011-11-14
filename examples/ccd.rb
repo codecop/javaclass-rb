6e3b210c9ea94a700088537b7ea04d11760990dc
Peter Kofler <peter.kofler@code-cop.org>
2011-11-14 01:29:37 +0100

    implement ccd like example

---
+++ examples/ccd.rb
@@ -0,0 +1,36 @@
+# Example usage of transitive dependencies.
+# Author::          Peter Kofler
+
+#--
+# add the lib of this gem to the load path
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require File.join(File.dirname(__FILE__), 'corpus')
+
+location = Corpus[:HBD]
+package = 'at.herold'
+start_class = 'at.herold.waf.hbd.servlet.HBDServlet'
+# 'at/spardat/krimiaps/service/client/service/impl/ClientSmeSearchServiceImpl'
+# 'at/spardat/krimiaps/service/client/service/impl/ClientPrivateSearchServiceImpl'
+
+#++
+require 'javaclass/dsl/mixin'
+
+# speed up loading by skipping non source file classpaths
+Eclipse.skip_lib
+
+# 1) create the (tracking) CompositeClasspath of the given workspace
+cp = workspace(location)
+puts "#{cp.count} classes found on classpath"
+
+# limit all operations to the classes we are interrested in
+filter = Proc.new { |classname| classname.same_or_subpackage_of?(package) }
+
+# 2a) collect all transitive dependencies from its package
+dependencies = cp.transitive_dependencies_package(start_class.to_javaname.package, &filter)
+puts "#{dependencies.size} classes in transitive dependency graph of package"
+# dependencies.debug_print
+
+# 2b) or collect all transitive dependencies from a single class
+dependencies = cp.transitive_dependency_tree(start_class.to_javaname, &filter)
+puts "#{dependencies.size} classes in transitive dependency graph of class"
+dependencies.debug_print
