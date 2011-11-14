6a9cd45e2d954b9f5f62faebdbeaaff6a0bd9046
Peter Kofler <peter.kofler@code-cop.org>
2011-11-14 01:23:58 +0100

    mix in the classpath analyser to the caching classpath as well

--- lib/javaclass/dsl/classpath_analysers.rb
+++ lib/javaclass/dsl/classpath_analysers.rb
@@ -1,15 +1,26 @@
 require 'javaclass/dsl/loading_classpath'
+require 'javaclass/dsl/caching_classpath'
 require 'javaclass/analyse/dependencies'
 require 'javaclass/analyse/transitive_dependencies'
 
 module JavaClass
   module Dsl
 
-    class LoadingClasspath
+    # A mixin for all class path analyser functions that need to be mixed into LoadingClasspath.
+    # Author::          Peter Kofler
+    module ClasspathAnalysers
       # add dependency analysis to LoadingClasspath
       include Analyse::Dependencies
       # add transitive dependency analysis to LoadingClasspath
       include Analyse::TransitiveDependencies
+    end
+
+    class LoadingClasspath
+      include ClasspathAnalysers
+    end
+
+    class CachingClasspath
+      include ClasspathAnalysers
     end
 
   end
