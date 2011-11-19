c9f4d5ae6b2b8c0a651d20f2a5b2ae018a319bed
Peter Kofler <peter.kofler@code-cop.org>
2011-11-20 00:20:32 +0100

    finally local links are working

--- lib/javaclass.rb
+++ lib/javaclass.rb
@@ -5,10 +5,9 @@ require 'javaclass/classscanner/scanners'
 # Main entry point for class file parsing. This provides all main methods as
 # class methods. For an alternative usage see JavaClass::Dsl::Mixin.
 # Author::          Peter Kofler
-# TODO change header of all main classes to contain copyright and license as well?
-# Author::          Peter Kofler
 # Copyright::       Copyright (c) 2009, Peter Kofler.       
-# License::         BSD License
+# License::         {BSD License}[link:/files/license_txt.html]
+# TODO ? change header of all main classes to contain copyright and license as well?
 
 module JavaClass
   extend Classpath::Factory
