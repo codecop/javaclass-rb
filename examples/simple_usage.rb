f7a802a1cc5e6e2cfdcf97370c2a307c185e7024
Peter Kofler <peter.kofler@code-cop.org>
2011-11-20 00:09:29 +0100

    finally local links are working, update/beautify RDoc

--- examples/simple_usage.rb
+++ examples/simple_usage.rb
@@ -1,25 +1,28 @@
 # Example of simple usage of JavaClass to load and inspect class files.
 # Author::          Peter Kofler
+#
+# === Usage
 
 #--
 # add the lib of this gem to the load path
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 #++
+# 1) require the basic module
 require 'javaclass'
 
-# 1) load a class directly from the file system
+# 2a) load a class directly from the file system
 clazz = JavaClass.load_fs('./test/data/access_flags/AccessFlagsTestPublic.class')
 
-# or get a class from the system classpath which needs JAVA_HOME to be set
+# 2b) or get a class from the system classpath which needs _JAVA_HOME_ to be set
 cp = JavaClass.environment_classpath
 puts cp.includes?('java/lang/String.class') # => 1 (true)
 
-# or look up the class from some classpath by its Java qualified name
+# 2c) or look up the class from some JavaClass::Classpath by its Java qualified name
 cp = JavaClass.classpath('./test/data/access_flags')
 puts cp.includes?('AccessFlagsTestPublic')  # => 1 (true)
 clazz = JavaClass.load_cp('AccessFlagsTestPublic', cp)
 
-# 2) retrieve low level information about a class
+# 3) then retrieve low level information about the class
 puts clazz.version                          # => "50.0"
 puts clazz.constant_pool.items[1]           # => "packagename/AccessFlagsTestPublic"
 puts clazz.access_flags.public?             # => true
@@ -30,12 +33,8 @@ puts clazz.super_class.to_classname         # => "java.lang.Object"
 puts clazz.references.referenced_methods[0] # => "java/lang/Object.<init>:()V"
 puts clazz.interfaces                       # => []
 
-# 3) work with class names
+# Returned class names are not just Strings, but JavaClass::JavaQualifiedName
 puts clazz.this_class.to_java_file          # => "packagename/AccessFlagsTestPublic.java"
 puts clazz.this_class.full_name             # => "packagename.AccessFlagsTestPublic"
 puts clazz.this_class.package               # => "packagename"
 puts clazz.this_class.simple_name           # => "AccessFlagsTestPublic"
-
-# TODO CONTINUE 5 - create a script that creates the *.txt for the good examples from rb and includes them in rdoc
-# (this is in fact a rake task)
-# add the links to them to readme.txt
