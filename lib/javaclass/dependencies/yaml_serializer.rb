579a617f8ee83a6e6f84aa48aaf6913b01276018
Peter Kofler <peter.kofler@code-cop.org>
2012-09-03 18:38:05 +0200

    add yaml deserializing

--- lib/javaclass/dependencies/yaml_serializer.rb
+++ lib/javaclass/dependencies/yaml_serializer.rb
@@ -1,33 +1,74 @@
 require 'yaml'
+require 'javaclass/dependencies/node'
+require 'javaclass/dependencies/graph'
 
 module JavaClass
   module Dependencies
-
+    
     # Serializes a Graph of Nodes to YAML.
     # Author::          Peter Kofler
     class YamlSerializer
-    
-      # Save the _graph_ to _filename_
+      
+      # Save the _graph_ to YAML _filename_
       def save(filename, graph)
         File.open(filename + '.yaml', 'w') { |f| f.print graph_to_yaml(graph) }
       end
 
       # Return a String of the YAML serialitzed _graph_
       def graph_to_yaml(graph)
-        "---\n" +
         graph.to_a.map { |node| node_to_yaml(node) }.join("\n")
       end
 
       # Return a String of the YAML serialitzed _node_
       def node_to_yaml(node)
-        '- ' + node.name + ":\n" +
-        node.dependencies.keys.map { |dep| 
-          '    ' + dep.name + ": ---\n" + dependencies_to_yaml(node.dependencies[dep]) 
+        node.name + ":\n" +
+        node.dependencies.keys.map { |dep|
+          '  ' + dep.name + ": ---\n" + dependencies_to_yaml(node.dependencies[dep])
         }.join("\n")
       end
-      
+
+      private
+
       def dependencies_to_yaml(dependencies)
-        dependencies.map { |dep| '    - ' + dep.to_s }.join("\n")
+        dependencies.map { |dep| '  - ' + dep.to_s }.join("\n")
+      end
+
+      def node_with(name)
+        node = @nodes_by_name[name]
+        if node
+          node
+        else
+          @nodes_by_name[name] = Node.new(name)
+        end
+      end
+
+      public
+
+      # Save the _graph_ from YAML _filename_
+      def load(filename)
+        yaml = YAML.load(filename + '.yaml')
+        yaml_to_graph(yaml)
+      end
+      
+      # Return a Graph from the YAML data _yaml_
+      def yaml_to_graph(yaml)
+        graph = Graph.new
+        @nodes_by_name = {}
+
+        yaml.keys.each do |name|
+          node = node_with(name)
+          graph.add(node)
+
+          dependency_map = yaml[name] || {}
+          dependency_map.keys.each do |dependency_name|
+            depending_node = node_with(dependency_name)
+            dependencies = dependency_map[dependency_name]
+            node.add_dependencies_to(dependencies, [depending_node])
+          end
+        end
+        
+        @nodes_by_name = {}
+        graph
       end
 
     end
--- test/test_node.rb
+++ test/test_node.rb
@@ -26,6 +26,17 @@ module TestJavaClass
         assert_equal(['dependency.for.other.node'], @node.dependencies[@other])
       end
 
+      def test_equal
+        assert_equal(@node, @node)
+        assert_equal(@node, JavaClass::Dependencies::Node.new('someNode'))
+        assert_not_equal(@node, @other)
+      end
+
+      def test_hash
+        assert_equal(@node.hash, @node.hash)
+        assert_equal(@node.hash, JavaClass::Dependencies::Node.new('someNode').hash)
+      end
+
     end
 
   end
