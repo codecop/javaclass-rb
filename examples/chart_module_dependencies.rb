40f5e904fbcdc206b269850907e2537d5ee2616a
Peter Kofler <peter.kofler@code-cop.org>
2012-10-15 21:15:21 +0200

    add description to example

--- examples/chart_module_dependencies.rb
+++ examples/chart_module_dependencies.rb
@@ -1,4 +1,5 @@
-# TODO desc.
+# Example usage of dependency graph: Iterate all folders of a root folder.
+# Load each component separately and construct the dependency graph.
 # Author::          Peter Kofler
 # Copyright::       Copyright (c) 2009, Peter Kofler.
 # License::         {BSD License}[link:/files/license_txt.html]
@@ -30,26 +31,35 @@ require 'javaclass/dependencies/classpath_node'
 require 'javaclass/dependencies/yaml_serializer'
 require 'javaclass/dependencies/graphml_serializer'
 
+# 1) define the location of the plugins
+#  location = 'C:\Eclipse\workspace'
+
 plugins = JavaClass::Dependencies::Graph.new
 
+# 2) iterate all plugins of a workspace location
 Dir.new(location).each do |folder|
+  #--
   next unless relatedPlugin?(folder)
-
+  #++
   path = File.join(location, folder)
   next unless FileTest.directory? path
 
   classes = File.join(path, 'bin')
   next unless FileTest.exist?(classes)
 
+  # 3) create a classpath for each plugin
   cp = classpath(classes)
   next if cp.count == 0
 
+  # 4) add a Node for that plugin to the Graph 
   plugins.add(JavaClass::Dependencies::ClasspathNode.new(folder, cp))
 end
+
+# 5) resolve all dependencies of all nodes in the Graph
 plugins.resolve_dependencies
 puts "#{plugins.to_a.size} plugins loaded" 
 
+# 6) save the result in various formats
 JavaClass::Dependencies::GraphmlSerializer.new.save('plugin_dependencies', plugins)
 JavaClass::Dependencies::YamlSerializer.new.save('plugin_dependencies', plugins)
 JavaClass::Dependencies::YamlSerializer.new(:summary).save('plugin_dependencies_summary', plugins)
-
