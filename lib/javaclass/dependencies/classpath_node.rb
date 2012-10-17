566b17662f5bdf4d2b4afe82e5ffa41f6078f17c
Peter Kofler <peter.kofler@code-cop.org>
2012-10-17 14:24:09 +0200

    add rdoc

--- lib/javaclass/dependencies/classpath_node.rb
+++ lib/javaclass/dependencies/classpath_node.rb
@@ -5,6 +5,8 @@ module JavaClass
   module Dependencies
     
     # A concrete Node which contains a Classpath and its dependencies.
+    # This models a Node as a component, maybe an Eclipse plugin, a Maven
+    # module or a library.
     # Author::          Peter Kofler
     class ClasspathNode < Node
     
