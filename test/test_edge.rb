65de0fde2fc0a69ecae9f1993f50e8dd987f54c2
Peter Kofler <peter.kofler@code-cop.org>
2012-10-17 23:06:51 +0200

    add support for uniq of Edges

---
+++ test/test_edge.rb
@@ -0,0 +1,60 @@
+require File.dirname(__FILE__) + '/setup'
+require 'javaclass/dependencies/edge'
+
+module TestJavaClass
+  module TestDependencies
+   
+    class TestEdge < Test::Unit::TestCase
+      include JavaClass::Dependencies
+    
+      def test_to_s
+        assert_equal('target (source)', Edge.new('source', 'target').to_s)
+      end
+
+      def test_equal_true
+        edge1 = Edge.new('blue', 'ball')
+        edge2 = Edge.new('blue', 'ball')
+        assert(edge1 == edge2)
+      end
+
+      def test_equal_false
+        edge1 = Edge.new('blue', 'ball')
+        edge2 = Edge.new('red', 'ball')
+        edge3 = Edge.new('blue', 'balloon')
+        assert(edge1 != edge2)
+        assert(edge1 != edge3)
+      end
+
+      def test_sort_of_list
+        original_list = [
+          Edge.new('2', 'b'),
+          Edge.new('1', 'b'),
+          Edge.new('2', 'a'),
+          Edge.new('1', 'a')
+        ].sort
+        sorted_list = [
+          Edge.new('1', 'a'),
+          Edge.new('2', 'a'),
+          Edge.new('1', 'b'),
+          Edge.new('2', 'b')
+        ]
+          
+        assert_equal(sorted_list, original_list.sort)
+      end
+
+      def test_uniq_of_list
+        original_list = [
+          Edge.new('1', 'a'),
+          Edge.new('1', 'a')
+        ].sort
+        uniq_list = [
+          Edge.new('1', 'a')
+        ]
+          
+        assert_equal(uniq_list, original_list.uniq)
+      end
+
+    end
+
+  end
+end
