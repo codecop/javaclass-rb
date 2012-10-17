2d73bf0dd7d6ecec3af8a781aabc49eaa8fa7bae
Peter Kofler <peter.kofler@code-cop.org>
2012-10-17 23:49:45 +0200

    ugly script to dependency graph analysis

--- examples/find_incoming_dependency_graph.rb
+++ examples/find_incoming_dependency_graph.rb
@@ -15,6 +15,7 @@ location = 'C:\RTC3.0\workspaces\Costing_Dev'
 #++
 require 'javaclass/dsl/mixin'
 require 'javaclass/classpath/tracking_classpath'
+require 'javaclass/dependencies/edge'
 require 'javaclass/dependencies/yaml_serializer'
 
 # e.g. {xxx}[link:/files/lib/generated/examples/find_unreferenced_classes_txt.html].
@@ -31,6 +32,7 @@ def strip(name)
   name.sub(/^com\.ibm\.arc\.sdm\./, '*.')
 end
 
+# A) mark all accessed classes in model plugin 
 plugins.
   to_a.
   reject { |node| node.name == 'com.ibm.arc.sdm.model' }.
@@ -39,17 +41,41 @@ plugins.
       next unless dep.name == 'com.ibm.arc.sdm.model'
       plugin.dependencies[dep].each do |edge|
         cp.mark_accessed(edge.target)
+      end
+    end
+end
+# TODO extract this method as visitor on graph that visits all edges going into "x" and yielding edge
+
+plugins.to_a.each do |plugin|
+  plugin.dependencies.keys.each do |dep|
+    plugin.dependencies[dep] = plugin.dependencies[dep].map do |edge| 
+      # replace class edges by package edges
+      # JavaClass::Dependencies::Edge.new(edge.source.to_javaname.package, edge.target.to_javaname.package)
+
+      # replace class edges with source plugin, target package
+      JavaClass::Dependencies::Edge.new(plugin.name, edge.target.to_javaname.package)
+    end.uniq.sort
+  end
+end
+
+plugins.
+  to_a.
+  reject { |node| node.name == 'com.ibm.arc.sdm.model' }.
+  each do |plugin|
+    plugin.dependencies.keys.each do |dep|
+      next unless dep.name == 'com.ibm.arc.sdm.model'
+      plugin.dependencies[dep].each do |edge|
         puts "#{strip(edge.target)};#{strip(plugin.name)};#{strip(edge.source)}"
       end
     end
 end
 
-# 1st find unused classes in model from outside
-unused_classes = classes.find_all { |clazz| cp.accessed(clazz) == 0 }.
-    reject { |clazz| clazz =~ /\$.*$/ }
-report = unused_classes.map { |clazz| "#{clazz.to_classname}" }.uniq
-# puts "#{report.size} private classes found:\n  #{report.join("\n  ")}"
-report.each do |clazz|
-  puts "#{strip(clazz)};(internal);xxx"
-end  
+## ad A) report unused classes in model from outside
+#unused_classes = classes.find_all { |clazz| cp.accessed(clazz) == 0 }.
+#    reject { |clazz| clazz =~ /\$.*$/ }
+#report = unused_classes.map { |clazz| "#{clazz.to_classname}" }.uniq
+## puts "#{report.size} private classes found:\n  #{report.join("\n  ")}"
+#report.each do |clazz|
+#  puts "#{strip(clazz)};(internal);NA"
+#end  
 
