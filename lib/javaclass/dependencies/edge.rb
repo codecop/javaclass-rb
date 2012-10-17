14453f3532e2ff0914b9592713422ecb9e145e30
Peter Kofler <peter.kofler@code-cop.org>
2012-10-17 22:09:29 +0200

    add rdoc

--- lib/javaclass/dependencies/edge.rb
+++ lib/javaclass/dependencies/edge.rb
@@ -1,7 +1,7 @@
 module JavaClass
   module Dependencies
     
-    # An edge in the graph of dependencies. An enge knows it's originatargetr and destination details.
+    # An edge in the Graph of dependencies. An edge knows it's source and destination details.
     # Author::          Peter Kofler
     class Edge
 
@@ -16,7 +16,11 @@ module JavaClass
       def to_s
         "#{@target} (#{@source})"
       end
-      
+
+      def ==(other)
+        @source == other.source && @target == other.target 
+      end
+            
       def <=>(other)
         res = @target <=> other.target
         if res == 0
