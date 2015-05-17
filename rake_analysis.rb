868710e59f2c1c01c334fe899fc6527db9f3159f
Peter Kofler <peter.kofler@code-cop.org>
2015-05-18 00:49:39 +0200

    ignore saikuro if not installed, skip hosting frolder from TODOs

--- rake_analysis.rb
+++ rake_analysis.rb
@@ -1,30 +1,41 @@
 require 'rake'
 require 'rake/clean' # for clean/clobber
-require File.dirname(__FILE__) + '/saikuro_task'
 
 # Static analysis functions.
 # Author::           Peter Kofler
 
 gemspec = eval(IO.readlines('javaclass.gemspec').join)
 
+begin
+  require File.dirname(__FILE__) + '/saikuro_task'
+
 # :complexity, :clobber_complexity, :recomplexity
 Rake::SaikuroTask.new do |saikuro|
   saikuro.files.include "#{gemspec.require_path}/**/*.rb"
 end
 
+rescue LoadError
+  # skip if not installed
+  warn("saikuro not installed. complexity not available. #{$!}")
+end
+
 # Helper method to grep all the sources for some _pattern_ words.
 def egrep(pattern)
-  Dir['**/*'].find_all { |fn| FileTest.file?(fn) && File.basename(fn) != /^\./ }.each do |fn|
-    line_count = 0
-    open(fn) do |f|
-      while line = f.gets
-        line_count += 1
-        if line =~ pattern
-          puts "#{fn}:#{line_count}:#{line.strip}"
+  Dir['**/*'].
+    find_all { |fn| FileTest.file?(fn) }.
+    reject { |fn| File.basename(fn) =~ /^\./ }.
+    reject { |fn| fn =~ /^hosting\// }.
+    each do |fn|
+      line_count = 0
+      open(fn) do |f|
+        while line = f.gets
+          line_count += 1
+          if line =~ pattern
+            puts "#{fn}:#{line_count}:#{line.strip}"
+          end
         end
       end
     end
-  end
 end
 
 desc 'Look for TODO and FIXME tags'
