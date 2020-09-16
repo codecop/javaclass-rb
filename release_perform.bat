d84761a6da18207a20b93aae8a700026a00df958
Peter Kofler <peter.kofler@code-cop.org>
2020-09-16 13:58:54 +0200

    Reduce unnecessary output of validate and perform scripts.

--- release_perform.bat
+++ release_perform.bat
@@ -1,56 +1,56 @@
 call rake clean clobber
 
-rem update the versions
+@rem update the versions
 call ued javaclass.gemspec
 call ued history.txt
-echo version number and history ok?
-pause
+@echo ===== version number and history ok?
+@pause
 del javaclass.gemspec.bak
 del history.txt.bak
 
-rem commit the new version
-call git commit -am "prepare release, update version"
+@rem commit the new version
+call git commit -am "Prepare next release, update version number."
 call rake tag
 call git log --stat -n 3
-echo tag mit version ok?
-pause
+@echo ===== tag with version ok?
+@pause
 
-rem publish gem
-set HOME=%HOMEDRIVE%%HOMEPATH%
+@rem publish gem
+@rem set HOME=%HOMEDRIVE%%HOMEPATH%
 call rake publish_gem
-echo publish to rubygem ok?
-pause
+@echo ===== publish to rubygem ok?
+@pause
 
-rem publish zip
-echo upload zip und gem manually!
-copy pkg\*.zip E:\Develop\Ruby\JavaClass\hosting\5_github.com\downloads\
-copy pkg\*.gem E:\Develop\Ruby\JavaClass\hosting\5_github.com\downloads\
+@rem publish zip
+@echo upload zip und gem manually!
+copy pkg\*.zip hosting\5_github.com\downloads\
+copy pkg\*.gem hosting\5_github.com\downloads\
 start https://www.code-cop.org/download/javaclass-rb/
-echo manual upload ok?
-pause
+@echo ===== manual upload ok?
+@pause
 
-rem publish doc
+@rem publish doc
 call rake publish_rdoc
 start api\index.html
-echo generated rdoc ok?
-set HOME=
-pause
+@echo ===== generated rdoc ok?
+@rem set HOME=
+@pause
 
-rem publish api doc
+@rem publish api doc
 echo upload newest API using FTP
-echo upload manually ok?
-pause
+echo ===== upload manually ok?
+@pause
 
-rem update the versions
+@rem update the versions
 call ued javaclass.gemspec
-echo next version number ok?
-pause
+@echo ===== next version number ok?
+@pause
 del javaclass.gemspec.bak
 
-rem commit the new version
-call git commit -am "after release, update version"
+@rem commit the new version
+call git commit -am "After release, update version number."
 call git log --stat -n 2
-echo commit ok?
-pause
+@echo ===== commit ok?
+@pause
 
 call rake clean clobber
--- release_validate.bat
+++ release_validate.bat
@@ -1,43 +1,44 @@
-cls
+@cls
 call rake clean clobber
 
-rem see if the code is finished
+@rem see if the code is finished
 call rake -f rake_analysis.rb todo
-echo ===== open todos ok?
-pause
+@echo ===== open todos ok?
+@pause
 
-rem see if the code works as expected
+@rem see if the code works as expected
 call rake validate_gem test rcov
 start coverage/index.html
-echo ===== test and coverage ok?
-pause
+@echo ===== test and coverage ok?
+@pause
 
-rem see if the code is complex
+@rem see if the code is complex
 call rake -f rake_analysis.rb
 start complexity/index_cyclo.html 
-echo ===== complexity ok?
-pause
+@echo ===== complexity ok?
+@pause
 
-rem validate the documentation
+@rem validate the documentation
 call rake fix_rdoc
 start html\index.html
-echo ===== RDOC ok?
-pause
+@echo ===== RDOC ok?
+@pause
 
-rem validate the package
-for /F "usebackq" %%a in (`call rake version`) do set VERS=%%a
+@rem validate the package
+@setlocal
+@for /F "usebackq" %%a in (`call rake version`) do set VERS=%%a
 call rake package
 cd pkg\javaclass-%VERS%
 call rake test
-echo ===== packaged test ok?
-pause
+@echo ===== packaged test ok?
+@pause
 
 call rake rdoc
 start html\index.html
-echo ===== packaged RDOC ok?
-cd ..\..
-set VERS=
-pause
+@echo ===== packaged RDOC ok?
+@cd ..\..
+@endlocal
+@pause
 
 call rake clean clobber
 call rake -f rake_analysis.rb clean clobber
